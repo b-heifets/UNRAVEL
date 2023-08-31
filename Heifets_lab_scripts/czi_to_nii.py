78b114eaf5d78118bbac02a3cd5ae7b6408fa828
Daniel Ryskamp Rijsketic <daniel.rijsketic@gmail.com>
2023-08-31 17:18:33 -0600

    Added unravel_utils.py and czi_to_nii.py

---
+++ Heifets_lab_scripts/czi_to_nii.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+
+import argparse
+import czifile
+import os
+import nibabel as nib
+import numpy as np
+from scipy import ndimage
+from glob import glob
+from rich import print
+import unravel_utils as unrvl
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Load channel of *.czi, downsample/resample, reorient, and save as .nii.gz')
+    parser.add_argument('-o', '--output', help='img.nii.gz', default="clar_res0.05.nii.gz", metavar='')
+    parser.add_argument('-c', '--channel', help='Channel number (0 for 1st channel)', default=0, type=int, metavar='')
+    parser.add_argument('-d', '--ds_factor', help='Downsampling factor', default=3, type=int,  metavar='')
+    parser.add_argument('-x', '--xy_res', help='x/y voxel size in microns', default=3.5232, type=float, metavar='') 
+    parser.add_argument('-z', '--z_res', help='x/y voxel size in microns', default=6, type=float, metavar='')
+    parser.add_argument('-r', '--res', help='Resample to this resolution (microns)', default=50, type=int, metavar='')
+    return parser.parse_args()
+
+args = parse_args() 
+
+
+
+def get_czi(sample_dirs): 
+    '''
+    Returns the path to the first .czi file in the sample folder
+    '''
+    czi_files = glob(f"{sample_dirs}/*.czi")
+    
+    if not czi_files:
+        print(f"  [red]No .czi found in {sample_dirs}[\]")
+        return None
+
+    return os.path.join(sample_dirs, czi_files[0])
+
+def load_channel_from_czi(czi_image, channel): 
+    '''
+    Loads the specified channel of the .czi file as a numpy array
+    '''
+    with czifile.CziFile(czi_image) as czi:
+        czi_array = czi.asarray()
+        image = czi_array[..., channel, :, :, :, :]
+        image = np.squeeze(image)
+        image = np.transpose(image, (2, 1, 0))
+        return image  
+
+def downsample(image, ds_factor):
+    '''
+    Quickly downsamples the image by the specified factor:
+    Drop voxels via slicing, keeping the middle voxel if the downscale factor is odd
+    '''
+    if ds_factor > 1:
+        start_index = ds_factor // 2 if ds_factor % 2 == 1 else (ds_factor - 1) // 2
+        image = image[start_index::ds_factor, start_index::ds_factor, start_index::ds_factor]
+    return image
+
+def resample(image, ds_factor, xy_res, z_res, target_resolution):
+    '''
+    Resamples the image to the specified resolution (in microns)
+    '''
+    if ds_factor == 1: # If not downsampling, use the original resolution
+        # Zoom factors:
+        zf_xy = float(xy_res)/float(target_resolution)
+        zf_z = float(z_res)/float(target_resolution)
+    else:
+        # Zoom factors:
+        zf_xy = float(xy_res * ds_factor)/float(target_resolution)
+        zf_z = float(z_res * ds_factor)/float(target_resolution)
+    resampled_img = ndimage.zoom(image, (zf_xy, zf_xy, zf_z), order=1) #order of 0 = NN interpolation, 1 for linear
+    return resampled_img
+
+def reorient(image):
+    '''
+    Reorients the image following MIRACL's convention
+    '''
+    reoriented_image = np.rollaxis(image, 0, 3)
+    return reoriented_image
+
+def make_output_nii_dir(sample_dirs):
+    '''
+    Creates a directory for the output .nii.gz file
+    '''
+    output_dir = sample_dirs / 'niftis'
+    os.makedirs(output_dir, exist_ok=True)
+
+def save_as_nii(sample_dirs, image, output_name, target_resolution):
+    '''
+    Saves the image as a .nii.gz file
+    '''
+    # Create new identity matrix
+    res = target_resolution / 1000 # convert to mm
+    affine = np.eye(4) * res
+    affine[3, 3] = 1
+
+    # Save as nii.gz
+    nifti = nib.Nifti1Image(image, affine)
+    nifti.header.set_data_dtype(np.int16)
+    nifti.header.set_zooms([res, res, res])
+    output = sample_dirs / 'niftis' / output_name
+    nib.save(nifti, output)
+
+
+
+@unrvl.function_decorator(message='') # Returns paths to sample folders and loops over them, passing each path to main()
+def load_czi_downsample_resample_save_nii(sample_dirs, channel, ds_factor, xy_res, z_res, target_resolution, output_name):
+    '''
+    Loads the specified channel of the .czi file, downsamples/resamples, reorients, and saves as .nii.gz
+    '''
+    # Load .czi file
+    czi_image = get_czi(sample_dirs)
+    if not czi_image:
+        return  # Exit the function if no .czi file is found
+    
+    # Load channel of .czi file
+    image = load_channel_from_czi(czi_image, channel)
+    
+    # Downsample
+    downsampled_image = downsample(image, ds_factor)
+
+    # Resample
+    resampled_image = resample(downsampled_image, ds_factor, xy_res, z_res, target_resolution)
+
+    # Reorient
+    reoriented_image = reorient(resampled_image)
+
+    # Make output directory
+    make_output_nii_dir(sample_dirs)
+
+    # Save as .nii
+    save_as_nii(sample_dirs, reoriented_image, output_name, target_resolution)
+
+
+
+@unrvl.main_function_decorator(pattern='sample??') # Adjust sample folder pattern as needed (e.g., 'sample??' for sample01, etc.)
+def main(sample_dirs):
+    args = parse_args() 
+
+    load_czi_downsample_resample_save_nii(sample_dirs, args.channel, args.ds_factor, args.xy_res, args.z_res, args.res, args.output)
+
+
+
+if __name__ == '__main__':
+    main()
+
+# Daniel Rijsketic 08/29-31/2023 (Heifets lab)
+# Wesley Zhao 08/30/2023 (Heifets lab)
+
+# To do:
+# Add option to get voxel size from metadata
+# Consider splitting up the load_czi_downsample_resample_save_nii function so that it can be reused for other scripts like the following:
+# Create alternate python script for starting from a tif series (e.g., data from UltraII microscope)
+# Does voxel_drop_middle already work correctly when xy_res and z_res are different? (e.g., 3.5232 and 6)
--- Heifets_lab_scripts/unravel_utils.py
+++ Heifets_lab_scripts/unravel_utils.py
@@ -1,13 +1,203 @@
 #!/usr/bin/env python3
 
+import fnmatch
+import functools
 import os
+import time
+import re
 import sys
-from termcolor import colored
 from datetime import datetime
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from rich.traceback import install
+from tqdm import tqdm
 
-def print_cmd():
-    print("\nRunning " +  colored(f"{os.path.basename(sys.argv[0])}", 'magenta', None, ['bold']) + f" starting at {datetime.now().strftime('%H:%M:%S')}\n")
-    cmd_args = [os.path.basename(sys.argv[0])] + sys.argv[1:]
-    print(colored(' '.join(cmd_args), 'cyan') + "\n")
+
+##############################################
+# Decorators for the main_function_decorator #
+##############################################
+
+def start_and_end_times(func):
+    """
+    A decorator that prints the start and end times of the function it decorates.
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = datetime.now()
+        print(f"  [bright_blue]Start:[/] " + start_time.strftime('%Y-%m-%d %H:%M:%S') + "\n")
+        result = func(*args, **kwargs)
+        end_time = datetime.now()
+        print(f"\n\n  :mushroom: [bold magenta]{os.path.basename(sys.argv[0])}[/] [purple3]finished[/] [bright_blue]at:[/] {end_time.strftime('%Y-%m-%d %H:%M:%S')}[gold3]![/][dark_orange]![/][red1]![/] \n")
+        
+        return result
+    return wrapper
+
+def print_cmd_decorator(func):
+    """
+    A decorator to print the script name and arguments used before running the decorated function.
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        cmd = f"\n\n[bold magenta]{os.path.basename(sys.argv[0])}[/] [purple3]{' '.join(sys.argv[1:])}[/]\n"
+        
+        console = Console()   # Instantiate the Console object
+        console.print(cmd)
+        
+        return func(*args, **kwargs)
+    return wrapper
+
+def rich_traceback(func):
+    """
+    A decorator that installs rich traceback for better exception visualization.
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        install()  # Enable the rich traceback
+        return func(*args, **kwargs)
     
-# Daniel Rijsketic 08/17/2023 (Heifets lab)
+    return wrapper
+
+def iterate_dirs(pattern=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+                
+            # Get all directories with the given pattern
+            dir_paths = [d for d in os.listdir(os.getcwd()) if (os.path.isdir(os.path.join(os.getcwd(), d)) and re.fullmatch(fnmatch.translate(pattern), d))]
+            dir_paths = sorted(dir_paths, key=lambda x: (len(x), x))  # Sort by length of string, then numerically
+            
+            print(f"\n  [bright_black]Processing these folders: {dir_paths}[/]")
+            
+            # Call the wrapped function for each directory path
+            for dir_name in tqdm(dir_paths, desc="  ", ncols=100, dynamic_ncols=True, unit="dir", leave=True):
+                dir_path = os.path.join(os.getcwd(), dir_name)   # Modified this variable name for clarity
+                print("\n\n\n\n  Processing: " + f"[gold3]{dir_name}[/]    ")
+                func(dir_path, **kwargs)
+            
+        return wrapper
+    return decorator
+
+def main_function_decorator(pattern=None):
+    def decorator(fn):
+        @functools.wraps(fn)
+        @print_cmd_decorator
+        @start_and_end_times
+        @rich_traceback
+        def wrapped(*args, **kwargs):
+            # Here, args[0] should be the root path based on how you've set up the script
+            root_path = args[0] if args else None
+
+            # Apply iterate_dirs to fn, so that fn (i.e., main) gets called for every matched directory
+            iterate_func = iterate_dirs(pattern=pattern)(fn)
+            
+            # Now call iterate_func with the root path
+            iterate_func(root_path)
+
+        return wrapped
+    return decorator
+
+
+#########################################
+# Decorators for the function_decorator #
+#########################################
+
+def timer(original_func_name): 
+    """
+    A decorator to time a function
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper_timer(*args, **kwargs):
+            # Convert args and kwargs to string for printing
+            args_str = ', '.join(str(arg) for arg in args)
+            kwargs_str = ', '.join(f"{k}={v}" for k, v in kwargs.items())
+            
+            # Combine both for final arguments string
+            all_args = args_str
+            if kwargs_str:
+                all_args += ', ' + kwargs_str
+                
+            print(f"\n  Running: [dark_orange]{original_func_name!r}[/] [bright_black]({all_args})[/]")
+            
+            start_time = time.perf_counter()
+            value = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            run_time = end_time - start_time
+            minutes, seconds = divmod(run_time, 60)
+            if minutes == 0:
+                print(f"\n  Finished in [orange_red1]{seconds:.4f}[/] seconds \n")
+            else:
+                print(f"\n  Finished in [orange_red1]{minutes:.0f}[/] minutes [orange_red1]{seconds:.4f}[/] seconds \n")
+            return value
+        return wrapper_timer
+    return decorator
+
+def task_status(message="Processing..."):
+    """
+    A decorator to show a console status icon in bold green for tasks that are processing.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            console = Console()
+            with console.status("[bold green]{}\n".format(message)) as status: # Added \n here
+                result = func(*args, **kwargs)
+                status.update("[bold green]Done!")
+            return result
+        return wrapper
+    return decorator
+
+def function_decorator(message=""):
+    """
+    A decorator that combines `iterate_dirs`, `timer`, and `task_status`.
+    """
+    def decorator(func):
+        original_func_name = func.__name__   # Capture the function name here
+        @functools.wraps(func)
+        @timer(original_func_name)
+        @task_status(message=message)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        
+        return wrapper
+    return decorator
+
+####################
+# Other decorators #
+####################
+
+def print_table(func):
+    """
+    A decorator that prints the result of the function it decorates in a table format using `rich`.
+    The decorated function should return a list of dictionaries, where each dictionary represents a row.
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        
+        # If result is empty or not a list, return the result without doing anything
+        if not result or not isinstance(result, list):
+            return result
+
+        # Assuming all dictionaries in the result have the same keys
+        headers = result[0].keys()
+        
+        table = Table(show_header=True, header_style="bold magenta")
+        
+        # Add columns for each header
+        for header in headers:
+            table.add_column(header)
+        
+        # Populate rows
+        for row_dict in result:
+            table.add_row(*[str(row_dict[col]) for col in headers])
+
+        console = Console()
+        console.print(table)
+        
+        return result
+    return wrapper
+
+# Daniel Rijsketic 08/17-30/2023 (Heifets lab)
+# Wesley Zhao 08/30/2021 (Heifets lab)
