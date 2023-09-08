b0aa2dfa963f4cd85562c75792ec0f27c8fa927d
daniel-rijsketic <daniel.rijsketic@gmail.com>
2023-09-07 21:13:44 -0600

    Finished removing downsampling from czi_to_reg_input.py. Now skips processing if output exists

--- Heifets_lab_scripts/czi_to_reg_input.py
+++ Heifets_lab_scripts/czi_to_reg_input.py
@@ -6,7 +6,6 @@ import os
 import nibabel as nib
 import numpy as np
 from glob import glob
-from glob import glob
 from pathlib import Path
 from rich import print
 from scipy import ndimage
@@ -16,14 +15,13 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Load channel of *.czi, resample, reorient, and save as .nii.gz')
     parser.add_argument('-o', '--output', help='img.nii.gz', default="clar_res0.05.nii.gz", metavar='')
     parser.add_argument('-c', '--channel', help='Channel number (0 for 1st channel)', default=0, type=int, metavar='')
-    parser.add_argument('-d', '--ds_factor', help='Downsampling factor', default=3, type=int,  metavar='')
     parser.add_argument('-x', '--xy_res', help='x/y voxel size in microns', default=3.5232, type=float, metavar='') 
     parser.add_argument('-z', '--z_res', help='x/y voxel size in microns', default=6, type=float, metavar='')
     parser.add_argument('-r', '--res', help='Resample to this resolution (microns)', default=50, type=int, metavar='')
+    parser.add_argument('-zo', '--zoom_order', help='Order of spline interpolation. Range: 0-5.)', default=1, type=int, metavar='')
     return parser.parse_args()
 
 args = parse_args() 
-
 
 def get_czi(sample_dirs): 
     '''
@@ -41,34 +39,27 @@ def load_channel_from_czi(czi_image, channel):
     '''
     Loads the specified channel of the .czi file as a numpy array
     '''
-    czi_image = CziFile(czi_image)
-    image = czi_image.read_image(C=args.channel)[channel] # Load the first channel of image | get img ([1] is the shape)
-    image = np.squeeze(image)
-    image = np.transpose(image, (2, 1, 0))
+    with CziFile(czi_image) as czi:
+        image = czi.read_image(C=channel)
+        image = np.squeeze(image)
+        image = np.transpose(image, (2, 1, 0))
     return image  
 
-def resample(image, ds_factor, xy_res, z_res, target_resolution):
+def resample(image, xy_res, z_res, target_resolution, zoom_order):
     '''
     Resamples the image to the specified resolution (in microns)
     '''
-    if ds_factor == 1: # If not downsampling, use the original resolution
-        # Zoom factors:
-        zf_xy = float(xy_res)/float(target_resolution)
-        zf_z = float(z_res)/float(target_resolution)
-    else:
-        # Zoom factors:
-        zf_xy = float(xy_res * ds_factor)/float(target_resolution)
-        zf_z = float(z_res * ds_factor)/float(target_resolution)
-    resampled_img = ndimage.zoom(image, (zf_xy, zf_xy, zf_z), order=1) #order of 0 = NN interpolation, 1 for linear
+    # Zoom factors:
+    zf_xy = float(xy_res)/float(target_resolution)
+    zf_z = float(z_res)/float(target_resolution)
+
+    resampled_img = ndimage.zoom(image, (zf_xy, zf_xy, zf_z), order=zoom_order) #order of 0 = NN interpolation, 1 for linear
     return resampled_img
 
 def reorient(image):
     '''
-    Reorients the image following MIRACL's convention
+    Reorients the image following MIRACL's convention in tiff to nii conversion
     '''
-    # reoriented_image = np.rollaxis(image, 0, 3)
-    # return reoriented_image
-
     # Rotate 90 degrees counterclockwise on the x-y plane
     rotated = np.rot90(image, axes=(1, 0))
 
@@ -101,10 +92,8 @@ def save_as_nii(sample_dirs, image, output_name, target_resolution):
     output = os.path.join(sample_dirs, "niftis", output_name)
     nib.save(nifti, output)
 
-
-
 @unrvl.function_decorator(message='') # Returns paths to sample folders and loops over them, passing each path to main()
-def load_czi_resample_save_nii(sample_dirs, channel, ds_factor, xy_res, z_res, target_resolution, output_name):
+def load_czi_resample_save_nii(sample_dirs, channel, xy_res, z_res, target_resolution, zoom_order, output_name):
     '''
     Loads the specified channel of the .czi file, resamples, reorients, and saves as .nii.gz
     '''
@@ -117,7 +106,7 @@ def load_czi_resample_save_nii(sample_dirs, channel, ds_factor, xy_res, z_res, t
     image = load_channel_from_czi(czi_image, channel)
 
     # Resample
-    resampled_image = resample(image, ds_factor, xy_res, z_res, target_resolution)
+    resampled_image = resample(image, xy_res, z_res, target_resolution, zoom_order)
 
     # Reorient
     reoriented_image = reorient(resampled_image)
@@ -128,28 +117,26 @@ def load_czi_resample_save_nii(sample_dirs, channel, ds_factor, xy_res, z_res, t
     # Save as .nii
     save_as_nii(sample_dirs, reoriented_image, output_name, target_resolution)
 
-
-
 @unrvl.main_function_decorator(pattern='sample??') # Adjust sample folder pattern as needed (e.g., 'sample??' for sample01, etc.)
 def main(sample_dirs):
     args = parse_args() 
 
-    load_czi_resample_save_nii(sample_dirs, args.channel, args.ds_factor, args.xy_res, args.z_res, args.res, args.output)
+    # Check if the output file already exists
+    output_file_path = os.path.join(sample_dirs, "niftis", args.output)
+    if os.path.exists(output_file_path):
+        print(f"Output file {output_file_path} already exists. Skipping.")
+        return  # Skip the rest of the main function for this sample_dirs
 
-
+    load_czi_resample_save_nii(sample_dirs, args.channel, args.xy_res, args.z_res, args.res, args.zoom_order, args.output)
 
 if __name__ == '__main__':
     main()
 
-# Daniel Rijsketic 08/29-31/2023 (Heifets lab)
-# Wesley Zhao 08/30/2023 (Heifets lab)
 
 # To do:
 # Add option to get voxel size from metadata
-# Consider splitting up the load_czi_resample_save_nii function so that it can be reused for other scripts like the following:
-# Create alternate python script for starting from a tif series (e.g., data from UltraII microscope)
-# Make the interpolation order for zoom an optional argument
-# with statement for czi_image
+# Create alternate python script for starting from a tif series (e.g., data from UltraII microscope), importing functions from this script
+
 # /SSD3/test/sample_w_czi/sample13/niftis/clar_res0.05_sh.nii.gz from 'prep_reg.sh 3.5232 6 488 8' is smoother, so we could try with an order of 2 or 3 with zoom for comparison
 
 # Notes: for testing:
