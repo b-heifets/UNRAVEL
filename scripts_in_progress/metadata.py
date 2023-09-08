292eaebfe70caa0af30a7c82f27819a33d99d0a7
daniel-rijsketic <daniel.rijsketic@gmail.com>
2023-09-08 10:35:52 -0600

    Added comments to metadata.py

--- scripts_in_progress/metadata.py
+++ scripts_in_progress/metadata.py
@@ -4,10 +4,10 @@
 # Description: Script to extract metadata from TIFF and CZI files and save to a params/metadata file
 # -----------------------------------------------------------------------------
 
-import os
-from pathlib import Path
+import os ### not used. Sorted imports alphabetically
 import tifffile
 from aicspylibczi import CziFile
+from pathlib import Path
 from lxml import etree
 
 def search_for_file(extensions, directories):
@@ -21,7 +21,7 @@ def search_for_file(extensions, directories):
     for directory in directories:
         for file in directory.iterdir():
             if file.name.endswith(extensions):
-                return Path(directory) / file
+                return Path(directory) / file 
     return None
 
 def rename_files_with_spaces(directory):
@@ -51,7 +51,7 @@ def get_metadata_from_tif(image_path):
     
             ome_xml_str = meta['ImageDescription'].value
             ome_xml_root = etree.fromstring(ome_xml_str.encode('utf-8'))
-            namespaces = {k: v for k, v in ome_xml_root.nsmap.items() if k is not None}
+            namespaces = {k: v for k, v in ome_xml_root.nsmap.items() if k is not None} ### not used
             default_ns = ome_xml_root.nsmap[None]
             pixels_element = ome_xml_root.find(f'.//{{{default_ns}}}Pixels')
             
@@ -81,10 +81,13 @@ def get_metadata_from_tif(image_path):
             x_res = meta['XResolution'].value[0]
             y_res = meta['YResolution'].value[0]
             z_res = 1 # currently unknown how to get Z voxel size from std tiff series
+            ### Rather than arbitrarily setting z_res to 1, which could lead to unexpected behavior, 
+            ### we could print a message telling the user that z_res is unknown
+            ### and to provide it manually when running scripts that require it
 
     return SizeX, SizeY, SizeZ, x_res, y_res, z_res
 
-def extract_resolution_in_microns(resolution):
+def extract_resolution_in_microns(resolution): ### not used. I am guessing microscopy images will never be in inches
     """
     Convert TIFF resolution to microns.
 
@@ -105,10 +108,10 @@ def get_metadata_from_czi(image_path):
     """
     czi = CziFile(image_path)
     size = czi.size
-    SizeZ, SizeY, SizeX = size[-3:]
+    SizeZ, SizeY, SizeX = size[-3:] ### could be consolidated to:  SizeZ, SizeY, SizeX = czi.size[-3:] # easy enough to understand
 
     # Default voxel size values
-    x_res, y_res, z_res = 1, 1, 1
+    x_res, y_res, z_res = 1, 1, 1 ### again, I would avoid abritrary default values like this and would instead print a message
 
     # Extract voxel sizes from XML metadata
     xml_root = czi.meta
@@ -134,7 +137,13 @@ def main():
         if czi_file:
             SizeX, SizeY, SizeZ, x_res, y_res, z_res = get_metadata_from_czi(czi_file)
         else:
-            tif_directories = [current_dir, current_dir / "488_original", current_dir / "488"]
+            ### with the old approach the first tif in 488_original would have metadata if that folder existed
+            ### with the old approach, if 488_original didn't exist, the first tif in 488 would have metadata
+            ### with the new approach, users should be able to specify the name of the folders containing the tifs
+            ### so they may call it autofl or perhaps they don't even have an autofl channel, so it would 
+            ### be called ochann, cfos, or whatever they want. We could add an argparse option for this
+            ### with current_dir / "488" as the default. 
+            tif_directories = [current_dir, current_dir / "488_original", current_dir / "488"] 
             for directory in tif_directories:
                 rename_files_with_spaces(directory)
             tif_file = search_for_file(('.tif', '.tiff'), tif_directories)
