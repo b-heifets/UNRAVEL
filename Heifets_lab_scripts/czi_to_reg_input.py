323708323961052da20d8c37ad4153cc391d085e
daniel-rijsketic <daniel.rijsketic@gmail.com>
2023-09-07 18:56:22 -0600

    Renamed czi_to_nii.py to czi_to_reg_input.py and removed downsampling function

--- Heifets_lab_scripts/czi_to_reg_input.py
+++ Heifets_lab_scripts/czi_to_reg_input.py
@@ -13,7 +13,7 @@ from scipy import ndimage
 import unravel_utils as unrvl
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Load channel of *.czi, downsample/resample, reorient, and save as .nii.gz')
+    parser = argparse.ArgumentParser(description='Load channel of *.czi, resample, reorient, and save as .nii.gz')
     parser.add_argument('-o', '--output', help='img.nii.gz', default="clar_res0.05.nii.gz", metavar='')
     parser.add_argument('-c', '--channel', help='Channel number (0 for 1st channel)', default=0, type=int, metavar='')
     parser.add_argument('-d', '--ds_factor', help='Downsampling factor', default=3, type=int,  metavar='')
@@ -23,7 +23,6 @@ def parse_args():
     return parser.parse_args()
 
 args = parse_args() 
-
 
 
 def get_czi(sample_dirs): 
@@ -47,16 +46,6 @@ def load_channel_from_czi(czi_image, channel):
     image = np.squeeze(image)
     image = np.transpose(image, (2, 1, 0))
     return image  
-
-def downsample(image, ds_factor):
-    '''
-    Quickly downsamples the image by the specified factor:
-    Drop voxels via slicing, keeping the middle voxel if the downscale factor is odd
-    '''
-    if ds_factor > 1:
-        start_index = ds_factor // 2 if ds_factor % 2 == 1 else (ds_factor - 1) // 2
-        image = image[start_index::ds_factor, start_index::ds_factor, start_index::ds_factor]
-    return image
 
 def resample(image, ds_factor, xy_res, z_res, target_resolution):
     '''
@@ -115,9 +104,9 @@ def save_as_nii(sample_dirs, image, output_name, target_resolution):
 
 
 @unrvl.function_decorator(message='') # Returns paths to sample folders and loops over them, passing each path to main()
-def load_czi_downsample_resample_save_nii(sample_dirs, channel, ds_factor, xy_res, z_res, target_resolution, output_name):
+def load_czi_resample_save_nii(sample_dirs, channel, ds_factor, xy_res, z_res, target_resolution, output_name):
     '''
-    Loads the specified channel of the .czi file, downsamples/resamples, reorients, and saves as .nii.gz
+    Loads the specified channel of the .czi file, resamples, reorients, and saves as .nii.gz
     '''
     # Load .czi file
     czi_image = get_czi(sample_dirs)
@@ -126,12 +115,9 @@ def load_czi_downsample_resample_save_nii(sample_dirs, channel, ds_factor, xy_re
     
     # Load channel of .czi file
     image = load_channel_from_czi(czi_image, channel)
-    
-    # Downsample
-    downsampled_image = downsample(image, ds_factor)
 
     # Resample
-    resampled_image = resample(downsampled_image, ds_factor, xy_res, z_res, target_resolution)
+    resampled_image = resample(image, ds_factor, xy_res, z_res, target_resolution)
 
     # Reorient
     reoriented_image = reorient(resampled_image)
@@ -148,7 +134,7 @@ def load_czi_downsample_resample_save_nii(sample_dirs, channel, ds_factor, xy_re
 def main(sample_dirs):
     args = parse_args() 
 
-    load_czi_downsample_resample_save_nii(sample_dirs, args.channel, args.ds_factor, args.xy_res, args.z_res, args.res, args.output)
+    load_czi_resample_save_nii(sample_dirs, args.channel, args.ds_factor, args.xy_res, args.z_res, args.res, args.output)
 
 
 
@@ -160,17 +146,12 @@ if __name__ == '__main__':
 
 # To do:
 # Add option to get voxel size from metadata
-# Consider splitting up the load_czi_downsample_resample_save_nii function so that it can be reused for other scripts like the following:
+# Consider splitting up the load_czi_resample_save_nii function so that it can be reused for other scripts like the following:
 # Create alternate python script for starting from a tif series (e.g., data from UltraII microscope)
-# Does voxel_drop_middle already work correctly when xy_res and z_res are different? (e.g., 3.5232 and 6)
 # Make the interpolation order for zoom an optional argument
+# with statement for czi_image
+# /SSD3/test/sample_w_czi/sample13/niftis/clar_res0.05_sh.nii.gz from 'prep_reg.sh 3.5232 6 488 8' is smoother, so we could try with an order of 2 or 3 with zoom for comparison
 
 # Notes: for testing:
 # cd /SSD3/test/sample_w_czi/
-# czi_to_nii.py #voxel drop downsample factor 3 (output renamed with d3)
-# czi_to_nii.py -d 1 #just zoom (output renamed with d1)
-# output: 
-# /SSD3/test/sample_w_czi/sample13/niftis/clar_res0.05.nii.gz
-# Either method takes 17 seconds
-# Image smoothness similar but not identical. Since it is just as fast to use zoom, we should probably drop voxel drop and add in an optional arg for the interpolation order
-# /SSD3/test/sample_w_czi/sample13/niftis/clar_res0.05_sh.nii.gz from 'prep_reg.sh 3.5232 6 488 8' is smoother, so we could try with an order of 2 or 3 with zoom for comparison
+
