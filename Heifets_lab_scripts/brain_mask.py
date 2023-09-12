570b7c1c8c744e2ae270409476c3db6eb975e761
daniel-rijsketic <daniel.rijsketic@gmail.com>
2023-09-11 18:36:46 -0600

    Minor fixes to new script: brain_mask.py

--- Heifets_lab_scripts/brain_mask.py
+++ Heifets_lab_scripts/brain_mask.py
@@ -13,14 +13,13 @@ def parse_args():
     parser.add_argument('--dir_list', help='Folders to process in working dir (e.g., sample01 sample02) (Default: process sample??) ', nargs='+', default=None, metavar='')
     parser.add_argument('-i', '--ilp', help='path/trained_ilastik_project.ilp', default=None, metavar='')
     parser.add_argument('-t', '--tif_dir', help='Name of folder with autofluo tifs', default="autofl_50um_tifs", metavar='')
-    parser.add_argument('-o', '--output', help='img.nii.gz (default: clar_res0.05.nii.gz)', default="clar_res0.05.nii.gz", metavar='')
     return parser.parse_args()
 
 
 @unrvl.print_func_name_args_status_duration()
 def load_img_resample_reorient_save_nii(sample_dir, args=None):
 
-    tif_input_dir = Path(sample_dir, "reg_input", args.tifs_dir)
+    tif_input_dir = Path(sample_dir, "reg_input", args.tif_dir)
 
     cmd = [
     'run_ilastik.sh',
@@ -28,19 +27,19 @@ def load_img_resample_reorient_save_nii(sample_dir, args=None):
     '--project', args.ilp,
     '--export_source', 'Simple Segmentation',
     '--output_format', 'tif',
-    '--output_filename_format', f'reg_input/{args.tifs_dir}_seg_ilastik/{{nickname}}.tif',
+    '--output_filename_format', f'reg_input/{args.tif_dir}_seg_ilastik/{{nickname}}.tif',
     tif_input_dir]
 
     print(f"  {cmd}")
     subprocess.run(cmd)
 
-    print(f"\n  Output: [default bold]reg_input/{args.tifs_dir}_seg_ilastik/[/]")
+    print(f"\n  Output: [default bold]reg_input/{args.tif_dir}_seg_ilastik/[/]")
 
 @unrvl.print_cmd_and_times
 def main():
     args = parse_args()
 
-    seg_path = Path("reg_input", f"{args.tifs_dir}_seg_ilastik")
+    seg_path = Path("reg_input", f"{args.tif_dir}_seg_ilastik")
 
     unrvl.process_samples_in_dir(load_img_resample_reorient_save_nii, sample_list=args.dir_list, sample_dirs_pattern=args.dir_pattern, output=seg_path, args=args)
 
