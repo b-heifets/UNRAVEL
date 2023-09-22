d8e62fda4b325388a1d8393529b1cca4626e61c0
daniel-rijsketic <daniel.rijsketic@gmail.com>
2023-09-22 15:14:30 -0600

    Updates: regional_IF_mean_intensities.py (can pass list of regions)

--- Heifets_lab_scripts/regional_IF_mean_intensities.py
+++ Heifets_lab_scripts/regional_IF_mean_intensities.py
@@ -6,17 +6,19 @@ import numpy as np
 import csv
 
 def parse_args():
-    DEFAULT_ATLAS = '/Users/Danielthy/Documents/_Heifets_lab_data/_UNRAVEL/testing/gubra_ano_combined_25um.nii.gz'
+    DEFAULT_ATLAS = '/usr/local/unravel/atlases/gubra/gubra_ano_combined_25um.nii.gz'
     parser = argparse.ArgumentParser(description='Measure mean intensity of immunofluorescence staining in brain regions.')
     parser.add_argument('-a', '--atlas', help='path/atlas.nii.gz', default=DEFAULT_ATLAS, metavar='')
     parser.add_argument('-i', '--input', help='path/atlas_space_immunofluorescence_image.nii.gz', required=True, metavar='')
     parser.add_argument('-o', '--output', help='path/name.csv', default=None, metavar='')
+    parser.add_argument('-r', '--regions', nargs='*', type=int, help='Space-separated list of region intensities to process', default=None)
+
     return parser.parse_args()
 
-def calculate_mean_intensity(atlas, image):
+def calculate_mean_intensity(atlas, image, regions=None):
     """Calculates mean intensity for each region in the atlas."""
 
-    print("\nCalculating mean immunofluorescence intensity for each region in the atlas...\n")
+    print("\n  Calculating mean immunofluorescence intensity for each region in the atlas...\n")
 
     # Filter out background
     valid_mask = atlas > 0
@@ -26,18 +28,25 @@ def calculate_mean_intensity(atlas, image):
     # Use bincount to get sums for each region
     sums = np.bincount(valid_atlas, weights=valid_image)
     counts = np.bincount(valid_atlas)
-    mean_intensities = sums / counts
+
+    # Suppress the runtime warning and handle potential division by zero
+    with np.errstate(divide='ignore', invalid='ignore'):
+        mean_intensities = sums / counts
+
     mean_intensities = np.nan_to_num(mean_intensities)
 
     # Convert to dictionary (ignore background)
     mean_intensities_dict = {i: mean_intensities[i] for i in range(1, len(mean_intensities))}
 
-    # Optional: Print results
+    # Filter the dictionary if regions are provided
+    if regions:
+        mean_intensities_dict = {region: mean_intensities_dict[region] for region in regions if region in mean_intensities_dict}
+
+    # Optional: Print results for the filtered regions
     for region, mean_intensity in mean_intensities_dict.items():
-        print(f"  Region: {region}\tMean intensity: {mean_intensity}")
+        print(f"    Region: {region}\tMean intensity: {mean_intensity}")
 
     return mean_intensities_dict
-
 
 def write_to_csv(data, output_file):
     """Writes the data to a CSV file."""
@@ -57,8 +66,11 @@ def main():
     atlas = atlas.get_fdata()
     img = img.get_fdata()
 
+    # Use the provided list of regional intensities (if any)
+    region_intensities = args.regions
+
     # Calculate mean intensity
-    mean_intensities = calculate_mean_intensity(atlas, img)
+    mean_intensities = calculate_mean_intensity(atlas, img, region_intensities)
 
     if args.output is None:
         args.output = args.input.replace('.nii.gz', '_regional_mean_intensities.csv')
