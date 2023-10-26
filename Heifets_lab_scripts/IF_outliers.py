bc9872329ac03afcfe9441f730077071394b1ed1
daniel.rijsketic <daniel.rijsketic@gmail.com>
2023-10-25 17:32:37 -0700

    New: IF_outliers.py to check for outliers based on bulk signal

---
+++ Heifets_lab_scripts/IF_outliers.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import argparse
+import glob
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from config import Configuration
+from rich import print
+from rich.live import Live
+from unravel_img_tools import load_nii
+from unravel_utils import print_cmd_and_times, print_func_name_args_times, get_progress_bar
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Load .nii.gz images matching pattern, get mean intensity of voxels in mask, and check for outliers (>3*SD +/- the mean)')
+    parser.add_argument('-p', '--pattern', help='Regex pattern in quotes for matching .nii.gz images', default=None, metavar='')
+    parser.add_argument('-m', '--mask', help='path/mask.nii.gz', default=None, metavar='')
+    parser.add_argument('-o', '--output', help='path/name.[pdf/png]. Default: means_in_mask.pdf ', default='means_in_mask.pdf', metavar='')
+    parser.add_argument('-v', '--verbose', help='Increase verbosity. Default: False', action='store_true', default=False)
+    parser.epilog = "From dir w/ input images run: IF_outliers.py; Prints and plots results"
+    return parser.parse_args()
+
+
+def mean_intensity_within_mask(image, mask):
+    return np.mean(image[mask > 0])
+
+def detect_outliers(values):
+    mean_val = np.mean(values)
+    std_dev = np.std(values)
+    lower_bound = mean_val - 3 * std_dev
+    upper_bound = mean_val + 3 * std_dev
+    
+    outliers = [(i, v) for i, v in enumerate(values) if v < lower_bound or v > upper_bound]
+    return outliers
+
+
+def main():
+
+    mask = load_nii(args.mask)
+
+    # Collect .nii.gz files matching the pattern
+    images = [f for f in glob.glob(args.pattern) if os.path.basename(f) != args.mask]
+
+    mean_values = []
+
+    # For each image, calculate the mean intensity value within the masked region.
+    progress = get_progress_bar(total_tasks=len(images))
+    task_id = progress.add_task("[red]Getting means...", total=len(images))
+    with Live(progress):
+        for idx, img in enumerate(images):
+            image = load_nii(img)
+            mean_intensity = mean_intensity_within_mask(image, mask)
+            mean_values.append(mean_intensity)
+            print(f"{idx} Mean in mask for {img}: {mean_intensity}")
+            progress.update(task_id, advance=1)
+
+    # Plot mean values
+    plt.scatter(range(len(mean_values)), mean_values)
+    plt.xlabel('Image Index')
+    plt.ylabel('Mean Intensity within mask')
+    plt.title('Mean Intensities within mask for each image')
+    plt.savefig(args.output)
+
+    # Detect outliers
+    outliers = detect_outliers(mean_values)
+    if outliers:
+        for idx, value in outliers:
+            print(f"Potential outlier: {images[idx]} with mean intensity value: {value}")
+    else:
+        print("No outliers detected!")
+
+
+if __name__ == "__main__":
+    from rich.traceback import install
+    install()
+    args = parse_args()
+    Configuration.verbose = args.verbose
+    print_cmd_and_times(main)()
