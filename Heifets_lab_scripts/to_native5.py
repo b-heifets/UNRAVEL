9c8d959bf0548a00459c69411c952214923619e8
daniel.rijsketic <daniel.rijsketic@gmail.com>
2024-02-06 15:58:49 -0800

    Specified interpolator (NN default)

--- Heifets_lab_scripts/to_native5.py
+++ Heifets_lab_scripts/to_native5.py
@@ -11,7 +11,7 @@ from scipy.ndimage import zoom
 
 from argparse_utils import SuppressMetavar, SM
 from unravel_config import Configuration
-from unravel_img_io import load_3D_img, load_image_metadata_from_txt, save_as_zarr, save_as_nii
+from unravel_img_io import load_image_metadata_from_txt, save_as_zarr, save_as_nii
 from unravel_img_tools import reverse_reorient_for_raw_to_nii_conv
 from unravel_utils import print_cmd_and_times, print_func_name_args_times
 
@@ -19,7 +19,9 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Warp atlas space image to tissue space, reorient, and scale to full resolution', formatter_class=SuppressMetavar)
     parser.add_argument('-m', '--moving_img', help='path/image.nii.gz to warp from atlas space', required=True, action=SM)
     parser.add_argument('-f', '--fixed_img', help='path/fixed_image.nii.gz (e.g., reg_final/clar_downsample_res25um.nii.gz)', required=True, action=SM)
+    parser.add_argument('-i', '--interpol', help='Interpolator for ants.apply_transforms (nearestNeighbor [default], genericLabel, linear)', default="nearestNeighbor", action=SM)
     parser.add_argument('-o', '--output', help='Save as path/native_image.zarr (fast) or path/native_image.nii.gz if provided', action=SM)
+    parser.add_argument('-d', '--dtype', help='Desired dtype for full res output (uint8, uint16). Default: moving_img.dtype', action=SM)
     parser.add_argument('-md', '--metadata', help='path/metadata.txt. Default: ./parameters/metadata.txt', default="./parameters/metadata.txt", action=SM)
     parser.add_argument('-p', '--reg_o_prefix', help='Registration output prefix. Default: allen_clar_ants', default='allen_clar_ants', action=SM)
     parser.add_argument('-t', '--transforms', help="Name of folder w/ transforms from registration. Default: clar_allen_reg", default="clar_allen_reg", action=SM)
@@ -32,7 +34,7 @@ def parse_args():
 
 Prereq: ./parameters/metadata.txt (prep_reg.py or metadata.py)
 
-Usage: to_native5.py -m <path/image_to_warp_from_atlas_space.nii.gz> -f <path/fixed_image.nii.gz> [-l]
+Usage: to_native5.py -m <path/image_to_warp_from_atlas_space.nii.gz> -f <path/fixed_image.nii.gz> -o <native>/native_<img>.nii.gz [-l -d uint8]
 
 Next script: native_clusters.py"""
     return parser.parse_args()
@@ -75,9 +77,8 @@ def scale_to_full_res(ndarray, full_res_dims, zoom_order=0):
     scaled_img = zoom(ndarray, zoom_factors, order=zoom_order) 
     return scaled_img
 
-
 @print_func_name_args_times()
-def warp_to_native(moving_img_path, fixed_img_path, transforms_dir, reg_output_prefix, reg_input_res, fixed_img_res, metadata_path, legacy=False):
+def warp_to_native(moving_img_path, fixed_img_path, transforms_dir, reg_output_prefix, reg_input_res, fixed_img_res, interpol, metadata_path, legacy, zoom_order, data_type):
     """Warp image from atlas space to full res native space"""
     # Load images for warping
     atlas_space_ants_img = ants.image_read(moving_img_path)
@@ -85,15 +86,19 @@ def warp_to_native(moving_img_path, fixed_img_path, transforms_dir, reg_output_p
 
     # Warp from atlas to native space
     transforms_dir = Path(transforms_dir).resolve()
+    deformation_field = str(Path(f'{transforms_dir}/{reg_output_prefix}1Warp.nii.gz'))
+    generic_affine_matrix = str(Path(f'{transforms_dir}/{reg_output_prefix}0GenericAffine.mat'))
+    intial_affine_matrix = str(Path(f'{transforms_dir}/init_tform.mat'))
     warped_ants_img = ants.apply_transforms(
         fixed=fixed_ants_img,
         moving=atlas_space_ants_img,
-        transformlist=[
-            f'{transforms_dir}/{reg_output_prefix}1Warp.nii.gz', 
-            f'{transforms_dir}/{reg_output_prefix}0GenericAffine.mat',
-            f'{transforms_dir}/init_tform.mat'
-        ]
+        transformlist=[deformation_field, generic_affine_matrix, intial_affine_matrix],
+        interpolator=interpol
     )
+
+    # Lower bit depth if specified
+    if data_type: 
+        warped_ants_img = warped_ants_img.astype(data_type)
 
     # Load resolutions and dimensions of full res image or scaling and to calculate how much padding to remove
     xy_res, z_res, x_dim, y_dim, z_dim = load_image_metadata_from_txt(metadata_path)
@@ -115,10 +120,15 @@ def warp_to_native(moving_img_path, fixed_img_path, transforms_dir, reg_output_p
     # Find img dims of warped image lacking padding
     crop_sizes = resampled_dims * zf
 
+    # Lower bit depth
+    if data_type: 
+        warped_img = warped_ants_img.numpy().astype(data_type)
+    else: 
+        atlas_space_nib_img = nib.load(moving_img_path) 
+        data_type = atlas_space_nib_img.get_data_dtype()
+        warped_img = warped_ants_img.numpy().astype(data_type) # convert to ndarray with original dtype
+
     # Perform cropping to remove padding
-    atlas_space_nib_img = nib.load(moving_img_path) 
-    data_type = atlas_space_nib_img.get_data_dtype()
-    warped_img = warped_ants_img.numpy().astype(data_type) # convert to ndarray with original dtype
     cropped_img = warped_img[
         crop_mins[0]:crop_mins[0] + crop_sizes[0],
         crop_mins[1]:crop_mins[1] + crop_sizes[1],
@@ -129,13 +139,14 @@ def warp_to_native(moving_img_path, fixed_img_path, transforms_dir, reg_output_p
         cropped_img = reverse_reorient_for_raw_to_nii_conv(cropped_img)
 
     # Scale to full resolution
-    native_img = scale_to_full_res(cropped_img, original_dimensions, zoom_order=args.zoom_order)
+    native_img = scale_to_full_res(cropped_img, original_dimensions, zoom_order)
+
     return native_img
 
 
 def main():
 
-    native_img = warp_to_native(args.moving_img, args.fixed_img, args.transforms, args.reg_o_prefix, args.reg_res, args.fixed_res, args.metadata, args.legacy)
+    native_img = warp_to_native(args.moving_img, args.fixed_img, args.transforms, args.reg_o_prefix, args.reg_res, args.fixed_res, args.interpol, args.metadata, args.legacy, args.zoom_order, args.dtype)
 
     # Save as .nii.gz or .zarr
     if args.output:
