cb20f94c7423ff0b2b9a063e4158b70d1fa77c7e
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-04-09 14:09:29 -0600

    Added option to scale data to a fixed range

--- Heifets_lab_scripts/nii_io.py
+++ Heifets_lab_scripts/nii_io.py
@@ -15,10 +15,11 @@ def parse_args():
     parser.add_argument('-i', '--input', help='path/img.nii.gz', required=True, action=SM)
     parser.add_argument('-d', '--data_type', help='Data type of output. For example: uint16 (numpy conventions)', required=True, action=SM)
     parser.add_argument('-o', '--output', help='path/new_img.nii.gz. Default: path/img_dtype.nii.gz', action=SM)
-    parser.add_argument('-s', '--scale', help='Scale the data to the range of the new data type. Default: False', action='store_true', default=False)
-    parser.add_argument('-b', '--binary', help='Convert to binary image. Default: False', action='store_true', default=False)
-    parser.add_argument('-z', '--zscore', help='Convert the range of z-scored data (use uint8 data type). Default: False', action='store_true', default=False)
-    parser.add_argument('-v', '--verbose', help='Increase verbosity. Default: False', action='store_true', default=False)
+    parser.add_argument('-f', '--fixed_scale', help='Scale data using fixed min and max values. Supply as "min,max"', default=None)
+    parser.add_argument('-s', '--scale', help='Scale the data to the range of the new data type', action='store_true', default=False)
+    parser.add_argument('-b', '--binary', help='Convert to binary image.', action='store_true', default=False)
+    parser.add_argument('-z', '--zscore', help='Convert the range of z-scored data (use uint8 data type).', action='store_true', default=False)
+    parser.add_argument('-v', '--verbose', help='Increase verbosity.', action='store_true', default=False)
     parser.epilog = """Example usage:     nii_dtype.py -i path/img.nii.gz -d float32
 Example usage for z-score scaling:     nii_dtype.py -i path/img.nii.gz -d uint8 -z
 
@@ -28,15 +29,16 @@ Possible numpy data types supported by NIfTI images:
     - Floating Point: float32, float64
     - Complex Numbers: complex64, complex128
 
-With standard scaling, the min intensity becomes dtype min and max intensity becomes dtype max. Every other intensity is scaled accordingly.
-
-With z-score scaling, the range of z-scored data from -3 to 3 is converted to 0 to 255.
+With --scale, the min intensity becomes dtype min and max intensity becomes dtype max. Every other intensity is scaled accordingly.
+With --binary, the image is binarized (0 or 1).
+With --zscore, the range of z-scored data from -3 to 3 is converted to 0 to 255.
+With --fixed_scale, the data is scaled using the provided min and max values.
 """
     return parser.parse_args()
 
 
 @print_func_name_args_times()
-def convert_dtype(ndarray, data_type, scale_mode='none', zscore_range=(-3, 3), target_range=(0, 255)):
+def convert_dtype(ndarray, data_type, scale_mode='none', fixed_scale_range=None, zscore_range=(-3, 3), target_range=(0, 255)):
     """
     Convert the data type of a ndarray and optionally scale the data.
 
@@ -44,6 +46,7 @@ def convert_dtype(ndarray, data_type, scale_mode='none', zscore_range=(-3, 3), t
     - ndarray: Input ndarray.
     - data_type: Target data type.
     - scale_mode: 'none', 'standard', or 'zscore'. Determines the scaling approach.
+    - fixed_scale_range: Tuple indicating the fixed range for scaling if scale_mode is 'fixed'.
     - zscore_range: Tuple indicating the z-score range for scaling if scale_mode is 'zscore'.
     - target_range: Tuple indicating the target range for the data type conversion.
 
@@ -55,6 +58,11 @@ def convert_dtype(ndarray, data_type, scale_mode='none', zscore_range=(-3, 3), t
             print("Applying standard scaling...")
             data_min, data_max = ndarray.min(), ndarray.max()
             ndarray = (ndarray - data_min) / (data_max - data_min) * (target_range[1] - target_range[0]) + target_range[0]
+        elif scale_mode == 'fixed' and fixed_scale_range:
+            min_val, max_val = fixed_scale_range
+            print(f"Applying fixed range scaling from {min_val} to {max_val}...")
+            ndarray = (ndarray - min_val) / (max_val - min_val) * (target_range[1] - target_range[0]) + target_range[0]
+            ndarray = np.clip(ndarray, target_range[0], target_range[1])
         elif scale_mode == 'zscore':
             print("Applying z-score based scaling (converting range from -3 to 3 to 0 to 255)...")
             scale = (target_range[1] - target_range[0]) / (zscore_range[1] - zscore_range[0])
@@ -62,7 +70,8 @@ def convert_dtype(ndarray, data_type, scale_mode='none', zscore_range=(-3, 3), t
             ndarray = ndarray * scale + offset
             ndarray = np.clip(ndarray, target_range[0], target_range[1])
 
-    if scale_mode == 'none' or not np.issubdtype(np.dtype(data_type), np.floating):
+    # Clip the data to the target range if the data type is integer
+    if np.issubdtype(np.dtype(data_type), np.integer):
         dtype_info = np.iinfo(data_type) if np.issubdtype(np.dtype(data_type), np.integer) else np.finfo(data_type)
         ndarray = np.clip(ndarray, dtype_info.min, dtype_info.max)
 
@@ -86,7 +95,7 @@ def main():
         img = np.where(img > 0, 1, 0)
 
     # Determine the scaling mode
-    scale_mode = 'standard' if args.scale else 'zscore' if args.zscore else 'none'
+    scale_mode = 'standard' if args.scale else 'fixed' if args.fixed_scale else 'zscore' if args.zscore else 'none'
 
     # Determine target range based on specified data type and scaling mode
     if args.zscore:
@@ -97,15 +106,19 @@ def main():
             # For uint8, map z-scores to the full range of the type
             target_range = (0, 255)
     else:
-        # For standard scaling or no scaling, derive range from the data type
         if np.issubdtype(np.dtype(args.data_type), np.integer):
             dtype_info = np.iinfo(args.data_type)
         else:
             dtype_info = np.finfo(args.data_type)
         target_range = (dtype_info.min, dtype_info.max)
 
+    # Prepare fixed scale range if specified
+    fixed_scale_range = None
+    if args.fixed_scale:
+        fixed_scale_range = tuple(float(x) for x in args.fixed_scale.split(','))
+
     # Convert the data type and optionally scale the data
-    new_img = convert_dtype(img, args.data_type, scale_mode=scale_mode, zscore_range=(-3, 3), target_range=target_range)
+    new_img = convert_dtype(img, args.data_type, scale_mode=scale_mode, fixed_scale_range=fixed_scale_range, target_range=target_range)
 
     # Update the header's datatype
     new_nii = nib.Nifti1Image(new_img, nii.affine, nii.header)
