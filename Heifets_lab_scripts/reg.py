c456d204eb9e8385a85e2af3c46143648cf895ab
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-04-09 18:13:18 -0600

    Cleaned up script

--- Heifets_lab_scripts/reg.py
+++ Heifets_lab_scripts/reg.py
@@ -57,11 +57,11 @@ prep_reg.py, [prep_brain_mask.py], & [brain_mask.py] for warping an average temp
 """
     return parser.parse_args()
 
-# TODO: Warp atlas to tisse for checking reg
 
 @print_func_name_args_times()
-def bias_correction(image_path, mask_path=None, shrink_factor=2, verbose=False, output_dir=None):
+def bias_correction(image_path, mask_path=None, shrink_factor=2, verbose=False):
     """Perform N4 bias field correction on a .nii.gz and return an ndarray
+
     Args:
         image_path (str): Path to input image.nii.gz
         mask_path (str): Path to mask image.nii.gz
@@ -77,44 +77,6 @@ def bias_correction(image_path, mask_path=None, shrink_factor=2, verbose=False, 
     ndarray = ants_img_corrected.numpy()
 
     return ndarray
-
-def set_nii_orientation(nii_img, target_axcodes, zero_origin=True, qform_code=1, sform_code=1):
-    """Set the orientation the the .nii.gz image header
-    
-    Arguments: 
-    nii_img: a NIfTI image object from nibabel
-    target_axcodes: a tuple of axis codes like ('R', 'A', 'S') or ('L', 'P', 'S')
-    """
-
-    img = nii_img.get_fdata(dtype=np.float32) 
-
-    # Determine the current orientation of the ndarray
-    current_ornt = io_orientation(nii_img.affine)
-
-    # Convert target axis codes to an orientation
-    target_ornt = axcodes2ornt(target_axcodes)
-
-    # Find the transformation needed
-    transformation_ornt = ornt_transform(current_ornt, target_ornt)
-
-    # Adjust the affine to match the new orientation
-    new_affine = inv_ornt_aff(transformation_ornt, img.shape) @ nii_img.affine
-
-    # Zero the origin:
-    if zero_origin:
-        for i in range(3):
-            new_affine[i,3] = 0     
-        
-    # Make the .nii.gz image with the adjusted header.
-    nii_img_oriented = nib.Nifti1Image(img, new_affine, nii_img.header)
-
-    # Set the header information
-    nii_img_oriented.header['xyzt_units'] = 10 # mm, s
-    nii_img_oriented.header['qform_code'] = qform_code
-    nii_img_oriented.header['sform_code'] = sform_code
-    nii_img_oriented.header['regular'] = b'r'
-
-    return nii_img_oriented
 
 
 def main(): 
@@ -160,7 +122,7 @@ def main():
 
                 # Create NIfTI, set header info, and save the registration input (reference image) 
                 print(f'\n    Setting header info for the registration input\n')
-                fixed_img = fixed_img.astype(np.float32) # Convert the fixed image to FLOAT32 for ANTs
+                fixed_img = fixed_img.astype(np.float32) # Convert the fixed image to FLOAT32 for ANTsPy
                 reg_inputs_fixed_img_nii = nib.Nifti1Image(fixed_img, fixed_img_nii.affine.copy(), fixed_img_nii.header)
                 reg_inputs_fixed_img_nii.set_data_dtype(np.float32)
 
@@ -226,11 +188,6 @@ def main():
                 # Save the warped moving image output
                 ants.image_write(reg['warpedmovout'], output)
                 print(f"\nTransformed moving image saved to: \n{output}")
-
-                # # Save the warped fixed image output
-                # warpedfixout = str(Path(reg_outputs_path, str(Path(args.fixed_img).name).replace(".nii.gz", "__warped_to_moving_img.nii.gz")))
-                # ants.image_write(reg['warpedfixout'], warpedfixout)
-                # print(f"Transformed fixed image saved to: \n{warpedfixout}\n")
 
             # Warp the atlas image to the tissue image for checking registration
             warped_atlas = str(Path(reg_outputs_path, str(Path(args.atlas).name).replace(".nii.gz", "_in_tissue_space.nii.gz")))
