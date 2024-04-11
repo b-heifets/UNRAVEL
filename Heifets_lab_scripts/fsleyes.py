ff7ab409f71adf26c6d81198b29bc8962203e81a
daniel.rijsketic <daniel.rijsketic@gmail.com>
2024-04-11 16:50:16 -0700

    Updated help

--- Heifets_lab_scripts/fsleyes.py
+++ Heifets_lab_scripts/fsleyes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/local/fsl/fslpython/envs/fslpython/bin/python
 
 import argparse
 import os
@@ -10,8 +10,8 @@ from argparse_utils import SuppressMetavar, SM
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Run fsleyes with given display range and files.', formatter_class=SuppressMetavar)
     parser.add_argument('-i', '--inputs', help='List of .nii.gz files to view or if empty all .nii.gz files in the current directory.', nargs='*', default=None)
-    parser.add_argument('-min', '--display_range_min', help='Minimum display range value.', type=float)
-    parser.add_argument('-max', '--display_range_max', help='Maximum display range value.', type=float)
+    parser.add_argument('-min', '--display_range_min', help='Minimum display range value.', default=0, type=float)
+    parser.add_argument('-max', '--display_range_max', help='Maximum display range value.', required=True, type=float)
     parser.add_argument('-a', '--atlas', help='path/atlas.nii.gz (Default: /usr/local/unravel/atlases/gubra/gubra_ano_combined_25um.nii.gz)', default='/usr/local/unravel/atlases/gubra/gubra_ano_combined_25um.nii.gz', action=SM)
     return parser.parse_args()
 
