5ceffb09025d32bca64185c3b13147ed22c9af4e
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-06-10 11:46:47 -0600

    conf.py: trying sys.path.insert(0, os.path.abspath('../..'))

--- unravel/docs/conf.py
+++ unravel/docs/conf.py
@@ -6,7 +6,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(1, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('..'))
 sys.path.append(os.path.abspath('../cluster_stats'))
 sys.path.append(os.path.abspath('../cluster_stats/effect_sizes'))
 sys.path.append(os.path.abspath('../core'))
