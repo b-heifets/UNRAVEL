523def9025f085ef08da0c5ddd4960fdc075d5f3
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-08-15 15:42:23 -0600

    New: gubra_to_CCF_50um.py for warping a points image from 50 um gubra space to 50 um CCF space.

--- _other/_legacy/gubra_to_CCF_50um.py
+++ _other/_legacy/gubra_to_CCF_50um.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 """
-Use gubra_to_CCF.py from UNRAVEL to warp an image from gubra 25 um space to Allen CCFv3 30 um space.
+Use gubra_to_CCF.py from UNRAVEL to warp an image from gubra 50 um space to Allen CCFv3 50 um space. 
+This is useful for warping cell centroids from gubra space to CCFv3 space.
 
 Usage:
 ------
     for s in sample?? ; do <path>/gubra_to_CCF.py -m ${s}/CCF_space/${s}_consensus_cell_centroids.nii.gz -o ${s}/CCF_space/${s}_consensus_cell_centroids.nii.gz ; done
 
+Next steps:
+``io_img_to_points``
+``img_resample_points``
+
 Note: 
-    We will use CCFv3 space for future analyses, so this script is used for converting gubra 25 um space to CCFv3 30 um space.
+    This script is used for converting gubra 50 um space to CCFv3 50 um space.
 
     Update default values for fixed_img, reg_outputs, fixed_reg_in, and interpol as needed.
 """
@@ -26,14 +31,14 @@ from unravel.warp.to_fixed import forward_warp
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=SuppressMetavar)
     # Required arguments
-    parser.add_argument('-m', '--moving_img', help='path/image.nii.gz to warp from 25 um Gubra atlas space', required=True, action=SM)
-    parser.add_argument('-o', '--output', help='path/image_CCF30.nii.gz', required=True, action=SM)
+    parser.add_argument('-m', '--moving_img', help='path/image.nii.gz to warp from 50 um Gubra atlas space', required=True, action=SM)
+    parser.add_argument('-o', '--output', help='path/image_CCF50.nii.gz', required=True, action=SM)
 
     # Optional arguments with default values
-    parser.add_argument('-f', '--fixed_img', help='Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/CCFv3-2017_ano_30um_w_fixes.nii.gz', default="/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/CCFv3-2017_ano_30um_w_fixes.nii.gz", action=SM)
+    parser.add_argument('-f', '--fixed_img', help='Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m_50um/CCFv3-2017_ano_50um_w_fixes.nii.gz', default="/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m_50um/CCFv3-2017_ano_50um_w_fixes.nii.gz", action=SM)
     parser.add_argument('-i', '--interpol', help='Interpolator for ants.apply_transforms (nearestNeighbor [default], multiLabel, linear, bSpline)', default="nearestNeighbor", action=SM)
-    parser.add_argument('-ro', '--reg_outputs', help="Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/reg_outputs", default="/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/reg_outputs", action=SM)
-    parser.add_argument('-fri', '--fixed_reg_in', help='Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/reg_outputs/CCFv3-2017_ano_30um_w_fixes__fixed_reg_input.nii.gz', default='/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/reg_outputs/CCFv3-2017_ano_30um_w_fixes__fixed_reg_input.nii.gz', action=SM)
+    parser.add_argument('-ro', '--reg_outputs', help="Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m_50um/reg_outputs", default="/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m_50um/reg_outputs", action=SM)
+    parser.add_argument('-fri', '--fixed_reg_in', help='Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m_50um/reg_outputs/CCFv3-2017_ano_50um_w_fixes__fixed_reg_input.nii.gz', default='/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m_50um/reg_outputs/CCFv3-2017_ano_50um_w_fixes__fixed_reg_input.nii.gz', action=SM)
     parser.add_argument('-v', '--verbose', help='Increase verbosity.', action='store_true', default=False)
     parser.epilog = __doc__
     return parser.parse_args()
