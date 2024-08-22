5464e495e873c4b0e65070731dc5d647455f51c3
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-08-22 11:20:27 -0600

    New: _other/_testing/get_samples.py for testing the get_samples() function in unravel/core/utils.py.

---
+++ _other/_testing/get_samples.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+Use ``path/get_samples.py`` from UNRAVEL to test the get_samples() function in unravel/core/utils.py.
+"""
+
+import argparse
+from rich.traceback import install
+
+from unravel.core.argparse_utils import SuppressMetavar, SM
+from unravel.core.config import Configuration 
+from unravel.core.utils import log_command, verbose_start_msg, verbose_end_msg, initialize_progress_bar, get_samples
+
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=SuppressMetavar)
+    parser.add_argument('-p', '--pattern', help='Pattern for directories to process. Default: sample??', default='sample??', action=SM)
+    parser.add_argument('-d', '--dirs', help='Paths to sample?? dirs and/or dirs containing them. Default: use current dir', nargs='*', default=None, action=SM)
+    parser.add_argument('-v', '--verbose', help='Increase verbosity.', action='store_true', default=False)
+    parser.epilog = __doc__
+    return parser.parse_args()
+
+@log_command
+def main():
+    install()
+    args = parse_args()
+    Configuration.verbose = args.verbose
+    verbose_start_msg()
+
+    sample_paths = get_samples(args.dirs, args.pattern, verbose=args.verbose)
+
+    verbose_end_msg()
+
+
+if __name__ == '__main__':
+    main()
