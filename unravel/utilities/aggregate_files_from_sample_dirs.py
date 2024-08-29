12e295ff7fc081811b4611f92bfacdb9960f99a5
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-08-29 14:01:23 -0600

    Testing fix for Sphinx warning

--- unravel/utilities/aggregate_files_from_sample_dirs.py
+++ unravel/utilities/aggregate_files_from_sample_dirs.py
@@ -6,12 +6,16 @@ Use ``utils_agg_files`` from UNRAVEL to aggregate files from sample?? directorie
 
 Usage:
 ------
-    utils_agg_files -g 'atlas_space/<asterisk>_cfos_rb4_30um_CCF_space_z_LRavg.nii.gz' [-td target_output_dir] [-d list of paths] [-p sample??] [-v]
+    utils_agg_files -g 'atlas_space/``*``_cfos_rb4_30um_CCF_space_z_LRavg.nii.gz' [-td target_output_dir] [-d list of paths] [-p sample??] [-v]
 
 Usage to prepend sample folder name to the name of files being copied:
 ----------------------------------------------------------------------
     utils_agg_files -g 'atlas_space/cfos_rb4_30um_CCF_space_z_LRavg.nii.gz' -a [-td target_output_dir] [-d list of paths] [-p sample??] [-v]
 """
+
+# Usage:
+# ------
+#     utils_agg_files -g 'atlas_space/``*``_cfos_rb4_30um_CCF_space_z_LRavg.nii.gz' [-td target_output_dir] [-d list of paths] [-p sample??] [-v]
 
 import shutil
 from pathlib import Path
