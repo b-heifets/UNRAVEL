8ec883afc549b21fbc654cfbf45ab81bd95ccb91
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-10-08 11:23:43 -0600

    Added arg to nii_info.py to control printing volume and intensity info (adds time for larger images)

--- unravel/image_io/nii_info.py
+++ unravel/image_io/nii_info.py
@@ -5,7 +5,7 @@ Use ``io_nii_info`` from UNRAVEL to load an .nii.gz image and print its data typ
 
 Usage:
 ------
-    i -i path/img.nii.gz [-u]
+    i -i path/img.nii.gz [-u] [-v]
 """
 
 import nibabel as nib
@@ -27,6 +27,7 @@ def parse_args():
 
     opts = parser.add_argument_group('Optional arguments')
     opts.add_argument('-u', '--unique', help='Print unique intensities. Default: False', action='store_true', default=False)
+    opts.add_argument('-v', '--verbose', help='Print volume and intenisty info. Default: False', action='store_true', default=False)
 
     return parser.parse_args()
 
@@ -51,10 +52,11 @@ def main():
     print(f'\nData type: [default bold]{data_type}')
 
     # Print intensity range
-    img = np.asanyarray(nii.dataobj, dtype=nii.header.get_data_dtype()).squeeze()
-    max_intensity = img.max()
-    min_intensity = img.min()
-    print(f'Intensity range: {min_intensity} to {max_intensity}')
+    if args.verbose:
+        img = np.asanyarray(nii.dataobj, dtype=nii.header.get_data_dtype()).squeeze()
+        max_intensity = img.max()
+        min_intensity = img.min()
+        print(f'Intensity range: {min_intensity} to {max_intensity}')
 
     # Print dimensions
     print(f'\nShape (x, y, z): {nii.shape}')
@@ -65,15 +67,18 @@ def main():
     print(f'Voxel sizes (µm): {voxel_sizes}')
 
     # Print number of non-zero voxels and total number of voxels
-    num_nonzero = np.count_nonzero(img)
-    num_voxels = np.prod(img.shape)
-    nonzero_volume_in_mm = (voxel_sizes[0] * voxel_sizes[1] * voxel_sizes[2]) * num_nonzero / 1e9
-    total_volume_in_mm = (voxel_sizes[0] * voxel_sizes[1] * voxel_sizes[2]) * num_voxels / 1e9
-    print(f'\nNumber of non-zero voxels: {num_nonzero} out of {num_voxels} ([default bold]{num_nonzero / num_voxels:.2%}[/])')
-    print(f'Volume of non-zero voxels: {nonzero_volume_in_mm:.2f} out of {total_volume_in_mm:.2f} ([default bold]mm^3[/])')
+    if args.verbose:
+        num_nonzero = np.count_nonzero(img)
+        num_voxels = np.prod(img.shape)
+        nonzero_volume_in_mm = (voxel_sizes[0] * voxel_sizes[1] * voxel_sizes[2]) * num_nonzero / 1e9
+        total_volume_in_mm = (voxel_sizes[0] * voxel_sizes[1] * voxel_sizes[2]) * num_voxels / 1e9
+        print(f'\nNumber of non-zero voxels: {num_nonzero} out of {num_voxels} ([default bold]{num_nonzero / num_voxels:.2%}[/])')
+        print(f'Volume of non-zero voxels: {nonzero_volume_in_mm:.2f} out of {total_volume_in_mm:.2f} ([default bold]mm^3[/])')
 
     # Print number of unique intensities
     if args.unique:
+        if not 'img' in locals():
+            img = np.asanyarray(nii.dataobj, dtype=nii.header.get_data_dtype()).squeeze()
         uniq_intensities = cluster_IDs(img, min_extent=1, print_IDs=False, print_sizes=False)
         print(f'\nUnique intensities (total: [default bold]{len(uniq_intensities)}):')
         uniq_intensities = cluster_IDs(img, min_extent=1, print_IDs=True, print_sizes=False)
