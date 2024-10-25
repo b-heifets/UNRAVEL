5e96bfeed8fc4f136618b87a15b7ebbcc63cda1e
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-10-25 16:24:15 -0600

    Made the script more generic (e.g., X image and Y image rather than expression image and stats image)

--- _other/drafts/correlation.py
+++ _other/drafts/correlation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Use ``_other/drafts/correlation.py`` from UNRAVEL to calculate the region-wise or voxel-wise correlation between MERFISH-CCF gene expression and a 3D stats map.
+Use ``_other/drafts/correlation.py`` from UNRAVEL to calculate the region-wise or voxel-wise correlation between two images.
 
 Note:
     - If an atlas is provided, the script will calculate the region-wise correlation.
@@ -12,7 +12,7 @@ Note:
     
 Usage:
 ------
-    _other/drafts/correlation.py -i path/to/stats_map.nii.gz -e path/to/gene_expression.nii.gz [-mas path/to/mask1.nii.gz path/to/mask2.nii.gz] [-a path/to/atlas.nii.gz] [-r 1 2 3] [-csv path/to/CCFv3-2020_regional_summary.csv] [-v]
+    _other/drafts/correlation.py -x path/x_axis_image.nii.gz -y path/y_axis_image.nii.gz [-mas path/mask1.nii.gz path/mask2.nii.gz] [-a path/atlas.nii.gz] [-r 1 2 3] [-csv path/CCFv3-2020_regional_summary.csv] [-v]
 """
 
 import numpy as np
@@ -33,15 +33,15 @@ from unravel.core.config import Configuration
 from unravel.core.utils import log_command, verbose_start_msg, verbose_end_msg
 from unravel.core.img_io import load_nii
 from unravel.region_stats.rstats_mean_IF import calculate_mean_intensity
-from unravel.voxel_stats.apply_mask import load_mask, apply_mask_to_ndarray
+from unravel.voxel_stats.apply_mask import load_mask
 
 
 def parse_args():
     parser = RichArgumentParser(formatter_class=SuppressMetavar, add_help=False, docstring=__doc__)
 
     reqs = parser.add_argument_group('Required arguments')
-    reqs.add_argument('-i', '--input', help='Path to a 3D stats map (.nii.gz)"', required=True, action=SM)
-    reqs.add_argument('-e', '--exp_map', help='Path to the gene expression NIfTI (.nii.gz) file', required=True, action=SM)
+    reqs.add_argument('-x', '--x_image', help='path/x_axis_image.nii.gz', required=True, action=SM)
+    reqs.add_argument('-y', '--y_image', help='path/y_axis_image.nii.gz', required=True, action=SM)
 
     opts = parser.add_argument_group('Optional arguments')
     opts.add_argument('-mas', '--masks', help='Paths to mask .nii.gz files to restrict analysis. Default: None', nargs='*', default=None, action=SM)
@@ -50,7 +50,8 @@ def parse_args():
     opts.add_argument('-r', '--regions', help='Space-separated list of region IDs to process. Default: process all IDs', nargs='*', type=int, default=None, action=SM)
     opts.add_argument('-csv', '--csv_path', help='CSV name or path/name.csv. Default: CCFv3-2020_regional_summary.csv', default='/Users/Danielthy/Documents/_GitHub/UNRAVEL_dev/unravel/core/csvs/CCFv3-2020_regional_summary.csv', action=SM)
     opts.add_argument('-p', '--plot', help='Plot correlation. Default: False', action='store_true', default=False)
-    opts.add_argument('-y', '--y_label', help='Y-axis label for the correlation plot. Default: "Statistical Map"', default='Statistical Map', action=SM)
+    opts.add_argument('-xl', '--x_label', help='X-axis label for the correlation plot. Default: "Image X mean intensity"', default='Image X mean intensity', action=SM)
+    opts.add_argument('-yl', '--y_label', help='Y-axis label for the correlation plot. Default: "Image Y mean intensity"', default='Image Y mean intensity', action=SM)
 
     general = parser.add_argument_group('General arguments')
     general.add_argument('-v', '--verbose', help='Increase verbosity. Default: False', action='store_true', default=False)
@@ -60,68 +61,65 @@ def parse_args():
 # TODO: Change /Users/Danielthy/Documents/_GitHub/UNRAVEL_dev/unravel/core/csvs/CCFv3-2020_regional_summary.csv to CCFv3-2020_regional_summary.csv after testing
 
 
-def compute_regionwise_correlation(exp_img, stats_img, atlas_img, mask_list=None, regions=None):
-    """Compute region-wise correlation between gene expression and a 3D stats map, restricted by masks."""
+def compute_regionwise_correlation(imgX, imgY, atlas_img, mask_list=None, regions=None, verbose=False):
+    """Compute region-wise correlation between two images, restricted by masks and region IDs."""
     # Apply mask(s) if provided
-    valid_voxels = np.ones(exp_img.shape, dtype=bool)
+    valid_voxels = np.ones(imgY.shape, dtype=bool)
     if mask_list:
         for mask_data in mask_list:
             valid_voxels = valid_voxels & mask_data.astype(bool)
 
     # Mask the images to keep only valid voxels
-    exp_img_masked = np.where(valid_voxels, exp_img, 0)
-    stats_img_masked = np.where(valid_voxels, stats_img, 0)
+    imgX_masked = np.where(valid_voxels, imgX, 0)
+    imgY_masked = np.where(valid_voxels, imgY, 0)
 
-    # Calculate the mean expression and stats values for each region (region: mean_intensity)
-    mean_intensities_dict_exp = calculate_mean_intensity(atlas_img, exp_img_masked, regions=regions)
-    mean_intensities_dict_p = calculate_mean_intensity(atlas_img, stats_img_masked, regions=regions)
+    # Calculate the mean intensity of the images in each region
+    imgX_mean_intensities_dict = calculate_mean_intensity(atlas_img, imgX_masked, regions=regions, verbose=verbose)
+    mean_intensities_dict_exp = calculate_mean_intensity(atlas_img, imgY_masked, regions=regions, verbose=verbose)
 
     # Convert the dictionaries to DataFrames
-    df_exp_img = pd.DataFrame(mean_intensities_dict_exp.items(), columns=['Region', 'Mean_Expression'])
-    df_stats_img = pd.DataFrame(mean_intensities_dict_p.items(), columns=['Region', 'Mean_Stat'])
+    imgX_df = pd.DataFrame(imgX_mean_intensities_dict.items(), columns=['Region', 'ImgX_Mean'])
+    imgY_df = pd.DataFrame(mean_intensities_dict_exp.items(), columns=['Region', 'ImgY_Mean'])
 
     # Merge the DataFrames on the region column
-    merged_df = pd.merge(df_exp_img, df_stats_img, on='Region')
+    merged_df = pd.merge(imgX_df, imgY_df, on='Region')
 
-    # Remove regions with zero mean expression
-    merged_df = merged_df[merged_df['Mean_Expression'] > 0]
+    # Compute the Pearson correlation between the mean intensities
+    correlation, p_value = pearsonr(merged_df['ImgX_Mean'], merged_df['ImgY_Mean'])
 
-    # Compute the Pearson correlation between the mean expression and stats values
-    correlation, p_value = pearsonr(merged_df['Mean_Expression'], merged_df['Mean_Stat'])
+    # Number of regions
+    n_of_regions = merged_df.shape[0]
 
-    # Number of voxels used for correlation
-    n_of_voxels = merged_df.shape[0]
+    return correlation, p_value, merged_df, n_of_regions
 
-    return correlation, p_value, merged_df, n_of_voxels
-
-def compute_voxelwise_correlation(exp_img, img, mask_list=None):
-    """Compute voxel-wise correlation between gene expression and a 3D map, restricted by masks."""
+def compute_voxelwise_correlation(imgX, imgY, mask_list=None):
+    """Compute voxel-wise correlation between two images, restricted by masks."""
 
     # Check that the images have the same shape
-    if exp_img.shape != img.shape:
+    if imgX.shape != imgY.shape:
         raise ValueError("\n    [red1]The two images must have the same shape\n")
 
     # Apply mask(s) if provided
-    valid_voxels = np.ones(exp_img.shape, dtype=bool)
+    valid_voxels = np.ones(imgX.shape, dtype=bool)
     if mask_list:
         for mask_data in mask_list:
             valid_voxels = valid_voxels & mask_data.astype(bool)
 
     # Extract values from valid voxels
-    exp_img_valid = exp_img[valid_voxels]
-    img_valid = img[valid_voxels]
+    imgX_valid = imgX[valid_voxels]
+    imgY_valid = imgY[valid_voxels]
 
     # Number of valid voxels
-    n_of_voxels = exp_img_valid.size
+    n_of_voxels = valid_voxels.size
 
     # Check if there are enough valid voxels for correlation
-    if exp_img_valid.size < 2:
+    if n_of_voxels < 2:
         raise ValueError("\n    [red1]Not enough valid voxels to perform correlation\n")
 
     # Perform Pearson correlation
-    correlation, p_value = pearsonr(exp_img_valid, img_valid)
+    correlation, p_value = pearsonr(imgX_valid, imgY_valid)
     
-    return correlation, p_value, exp_img_valid, img_valid, n_of_voxels
+    return correlation, p_value, imgX_valid, imgY_valid, n_of_voxels
 
 
 def color_voxels_by_region(atlas_img, valid_voxels, region_csv_path):
@@ -145,32 +143,32 @@ def color_voxels_by_region(atlas_img, valid_voxels, region_csv_path):
 
     return rgb_colors
 
-
-def plot_correlation(expression_series, p_series, correlation, p_value, plot_title='Correlation Scatter Plot', rgb_colors=None, y_label='Statistical Map'):
-    """Generate a scatter plot for the given expression and a stats value series, with a trend line and colored points."""
+def plot_correlation(imgX_valid, imgY_valid, correlation, p_value, plot_title='Correlation Scatter Plot', rgb_colors=None, x_label=None, y_label=None):
+    """Generate a scatter plot for the correlation between two images."""
     plt.figure(figsize=(8, 6))
 
     # Scatter plot with RGB colors
     if rgb_colors is not None:
-        plt.scatter(expression_series, p_series, color=rgb_colors, alpha=0.7, label='Data points')
+        plt.scatter(imgX_valid, imgY_valid, color=rgb_colors, alpha=0.7, label='Data points')
     else:
-        plt.scatter(expression_series, p_series, color='blue', alpha=0.7, label='Data points')
+        plt.scatter(imgX_valid, imgY_valid, color='blue', alpha=0.7, label='Data points')
 
     # Fit and plot the trend line
-    z = np.polyfit(expression_series, p_series, 1)
+    z = np.polyfit(imgX_valid, imgY_valid, 1)
     p = np.poly1d(z)
-    plt.plot(expression_series, p(expression_series), color='black', linestyle='-', label='Trend line')
-
-    # Add a horizontal dashed line
-    # plt.axhline(y=0.95, color='grey', linestyle='--', label='1-p = 0.95')
+    plt.plot(imgX_valid, p(imgX_valid), color='black', linestyle='-', label='Trend line')
 
     # Add titles and labels
     plt.title(f'{plot_title}\nPearson r: {correlation:.2f}, p-value: {p_value:.2e}')
-    plt.xlabel('Gene Expression')
+    plt.xlabel(x_label)
     plt.ylabel(y_label)
     
     # Remove grid lines within the graph
     plt.grid(False)
+
+    # if there are negative values, add dashed lines at 0
+    plt.axhline(0, color='grey', linestyle='--')
+    plt.axvline(0, color='grey', linestyle='--')
 
     # Show the legend for the data points, trend line, and horizontal line
     plt.legend()
@@ -190,9 +188,8 @@ def main():
     Configuration.verbose = args.verbose
     verbose_start_msg()
 
-    # Load Htr2a expression and 3D map NIfTI files
-    exp_img = load_nii(args.exp_map)
-    img = load_nii(args.input)
+    imgX = load_nii(args.x_image)
+    imgY = load_nii(args.y_image)
 
     # Load masks if provided
     mask_list = []
@@ -203,11 +200,11 @@ def main():
     if args.regional and args.atlas:
         atlas_img = load_nii(args.atlas)
         # Compute the region-wise correlation
-        correlation, p_value, merged_df, n_of_voxels = compute_regionwise_correlation(exp_img, img, atlas_img, mask_list, args.regions)
+        correlation, p_value, merged_df, n = compute_regionwise_correlation(imgX, imgY, atlas_img, mask_list, args.regions, args.verbose)
 
     else:
         # Compute the voxel-wise correlation
-        correlation, p_value, exp_img_valid, img_valid, n_of_voxels = compute_voxelwise_correlation(exp_img, img, mask_list)
+        correlation, p_value, imgX_valid, imgY_valid, n = compute_voxelwise_correlation(imgX, imgY, mask_list)
 
     # Output the result
     # print(f"\n    Pearson correlation: {correlation}")
@@ -215,9 +212,7 @@ def main():
     # print(f"    Number of voxels: {n_of_voxels}\n")
     print(f"Pearson correlation,{correlation}")
     print(f"p-value,{p_value}")
-    print(f"Number of voxels,{n_of_voxels}")
-
-
+    print(f"Number of data points,{n}")
 
     # Plot the correlation scatter plot
     if args.plot:
@@ -237,15 +232,15 @@ def main():
             # Convert the R, G, B values into a format usable for matplotlib
             rgb_colors = merged_df[['R', 'G', 'B']].values / 255.0
 
-            plot_title = 'Region-wise Correlation Scatter Plot'
-            plot_correlation(merged_df['Mean_Expression'], merged_df['Mean_Stat'], correlation, p_value, plot_title, rgb_colors=rgb_colors, y_label=args.y_label)
+            plot_title = 'Region-wise Correlation'
+            plot_correlation(merged_df['ImgX_Mean'], merged_df['ImgY_Mean'], correlation, p_value, plot_title, rgb_colors=rgb_colors, x_label=args.x_label, y_label=args.y_label)
         else:
             if args.atlas:
                 # Load the atlas image
                 atlas_img = load_nii(args.atlas)
 
                 # Check for valid voxels in both images
-                valid_voxels = np.ones(exp_img.shape, dtype=bool)
+                valid_voxels = np.ones(imgX.shape, dtype=bool)
                 if mask_list:
                     for mask_data in mask_list:
                         valid_voxels = valid_voxels & mask_data.astype(bool)
@@ -254,11 +249,12 @@ def main():
                 rgb_colors = color_voxels_by_region(atlas_img, valid_voxels=valid_voxels, region_csv_path=args.csv_path)
         
                 # Plot the voxel-wise correlation with RGB colors
-                plot_title = 'Voxel-wise Correlation Scatter Plot'
-                plot_correlation(exp_img_valid, img_valid, correlation, p_value, plot_title, rgb_colors=rgb_colors, y_label=args.y_label)
+                plot_title = 'Voxel-wise Correlation'
+
+                plot_correlation(imgX_valid, imgY_valid, correlation, p_value, plot_title, rgb_colors=rgb_colors, x_label=args.x_label, y_label=args.y_label)
             else:
-                plot_title = 'Voxel-wise Correlation Scatter Plot'
-                plot_correlation(exp_img_valid, img_valid, correlation, p_value, plot_title, y_label=args.y_label)
+                plot_title = 'Voxel-wise Correlation'
+                plot_correlation(imgX_valid, imgY_valid, correlation, p_value, plot_title, x_label=args.x_label, y_label=args.y_label)
 
     verbose_end_msg()
 
