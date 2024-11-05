fd9d13270aba9ea1621fae89beb36dca039e1829
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-11-05 08:29:47 -0700

    Added cells_with_genes_df and removed some notes

--- _other/drafts/Allen_RNAseq_expression_in_cell_types.py
+++ _other/drafts/Allen_RNAseq_expression_in_cell_types.py
@@ -46,6 +46,32 @@ def parse_args():
 
     return parser.parse_args()
 
+
+Index(['Adrb2', 'Adrb1', 'cell_barcode', 'barcoded_cell_sample_label',
+       'library_label', 'feature_matrix_label', 'entity',
+       'brain_section_label', 'library_method', 'region_of_interest_acronym',
+       'donor_label', 'donor_genotype', 'donor_sex', 'dataset_label', 'x', 'y',
+       'cluster_alias'],
+
+Adrb2                                                          0.0
+Adrb1                                                     4.386587
+cell_barcode                                      GCCCGAAGTCAGTTTG
+barcoded_cell_sample_label                                 344_C04
+library_label                                   L8TX_200827_01_E10
+feature_matrix_label                               WMB-10Xv3-CTXsp
+entity                                                        cell
+brain_section_label                                            NaN
+library_method                                               10Xv3
+region_of_interest_acronym                                   CTXsp
+donor_label                           Snap25-IRES2-Cre;Ai14-539605
+donor_genotype                Snap25-IRES2-Cre/wt;Ai14(RCL-tdT)/wt
+donor_sex                                                        M
+dataset_label                                            WMB-10Xv3
+x                                                         6.320394
+y                                                         8.719297
+cluster_alias                                                 1001
+Name: GCCCGAAGTCAGTTTG-344_C04, dtype: object
+
 @log_command
 def main():
     install()
@@ -55,22 +81,23 @@ def main():
 
     download_base = Path(args.base)
 
+    # Extra columns for humans: cluster_alias, region_of_interest_label, anatomical_division_label
     cell_df = load_RNAseq_cell_metadata(download_base, species=args.species) # Add option to load cell_metadata_with_cluster_annotation.csv instead? Does this just add extra columns?
 
     # Load gene expression data
     gene_expression = pd.read_csv(args.input, index_col=0)
 
     # Join the cell metadata with the gene expression data
-    gene_expression = gene_expression.join(cell_df)
+    cells_with_genes = cell_df.join(gene_expression)
 
     # Print the head of the joined data
-    print(gene_expression.head())
+    print(cells_with_genes.head())
 
     # Print all columns
-    print(gene_expression.columns)
+    print(cells_with_genes.columns)
 
     # Print values of the first first row after the header
-    print(gene_expression.iloc[0])
+    print(cells_with_genes.iloc[0])
 
 
     verbose_end_msg()
