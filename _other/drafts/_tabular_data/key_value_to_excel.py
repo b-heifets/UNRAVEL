04d96db4d7930dee27ae6e872affafc9f0bd990b
daniel-rijsketic <daniel.rijsketic@gmail.com>
2024-12-03 15:56:10 -0700

    Makes path to parent of output file path

--- _other/drafts/_tabular_data/key_value_to_excel.py
+++ _other/drafts/_tabular_data/key_value_to_excel.py
@@ -107,7 +107,7 @@ def key_val_to_excel(data, output_path, delimiter=","):
         The delimiter used to separate keys and values. Default is ','.
     """
     df = parse_key_value_data(data, delimiter)
-    Path(output_path).mkdir(parents=True, exist_ok=True)
+    Path(output_path).parent.mkdir(parents=True, exist_ok=True)
     df.to_excel(output_path, index=False)
 
 
