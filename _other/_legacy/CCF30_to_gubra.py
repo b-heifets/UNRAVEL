9779b0306379ec8a87ee74c023c09de912fff153
daniel-rijsketic <daniel.rijsketic@gmail.com>
2025-02-21 12:39:26 -0700

    New: _other/_legacy/CCF30_to_gubra.py

--- _other/_legacy/CCF30_to_gubra.py
+++ _other/_legacy/CCF30_to_gubra.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
 """
-Use ``gubra_to_CCF.py`` from UNRAVEL to warp an image from gubra 25 um space to Allen CCFv3 30 um space.
-
-Note: 
-    - We will use CCFv3 space for future analyses, so this script is used for converting gubra 25 um space to CCFv3 30 um space.
+Use ``CCF30_to_gubra.py`` from UNRAVEL to warp an image from Allen CCFv3 30 um space to gubra 25 um space.
 
 Usage:
 ------
-``gubra_to_CCF.py`` -m path/image.nii.gz -o path/image_CCF30.nii.gz [-f path/CCFv3-2017_ano_30um_w_fixes.nii.gz] [-inp linear] [-ro path/reg_outputs] [-fri path/fixed_reg_input.nii.gz] [-v]
+``CCF30_to_gubra.py`` -m path/CCF30_image.nii.gz -o path/image_gubra_space.nii.gz [-a path/atlas.nii.gz] [-inp nearestNeighbor] [-fri path/fixed_reg_input.nii.gz] [-pad 0.15] [-dt uint16] [-v]
 """
 
 from pathlib import Path
@@ -16,24 +13,26 @@ from rich import print
 from rich.traceback import install
 
 from unravel.core.help_formatter import RichArgumentParser, SuppressMetavar, SM
+from unravel.core.img_io import load_nii
 from unravel.core.config import Configuration
 from unravel.core.utils import log_command, verbose_start_msg, verbose_end_msg
 
-from unravel.warp.to_fixed import forward_warp
+from unravel.warp.to_atlas import to_atlas
+
 
 def parse_args():
     parser = RichArgumentParser(formatter_class=SuppressMetavar, add_help=False, docstring=__doc__)
     
     reqs = parser.add_argument_group('Required arguments')
-    reqs.add_argument('-m', '--moving_img', help='path/image.nii.gz to warp from 25 um Gubra atlas space', required=True, action=SM)
-    reqs.add_argument('-o', '--output', help='path/image_CCF30.nii.gz', required=True, action=SM)
+    reqs.add_argument('-m', '--moving_img', help='path/image.nii.gz to warp from CCFv3 30 um space', required=True, action=SM)
+    reqs.add_argument('-o', '--output', help='path/image_gubra_space.nii.gz', required=True, action=SM)
 
     opts = parser.add_argument_group('Optional arguments')
-    opts.add_argument('-inp', '--interpol', help='Interpolator for ants.apply_transforms (nearestNeighbor \[default], multiLabel, linear, bSpline)', default="nearestNeighbor", action=SM)
-    opts.add_argument('-f', '--fixed_img', help='Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/CCFv3-2017_ano_30um_w_fixes.nii.gz', default="/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/CCFv3-2017_ano_30um_w_fixes.nii.gz", action=SM)
-    opts.add_argument('-ro', '--reg_outputs', help="Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/reg_outputs", default="/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/reg_outputs", action=SM)
+    opts.add_argument('-a', '--atlas', help='path/atlas.nii.gz or template matching moving image. Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/gubra_ano_combined_25um_w_fixes.nii.gz', default='/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/gubra_ano_combined_25um_w_fixes.nii.gz', action=SM)
+    opts.add_argument('-inp', '--interpol', help='Interpolator for ants.apply_transforms (multiLabel \[default],  nearestNeighbor, linear, bSpline)', default="multiLabel", action=SM)
     opts.add_argument('-fri', '--fixed_reg_in', help='Default: /usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/reg_outputs/CCFv3-2017_ano_30um_w_fixes__fixed_reg_input.nii.gz', default='/usr/local/unravel/atlases/gubra_to_CCF/CCF-f__Gubra-m/reg_outputs/CCFv3-2017_ano_30um_w_fixes__fixed_reg_input.nii.gz', action=SM)
     opts.add_argument('-pad', '--pad_percent', help='Percentage of padding that was added to each dimension of the fixed image during ``reg``. Default: 0.15 (15%%).', default=0.15, type=float, action=SM)
+    opts.add_argument('-dt', '--dtype', help='Desired dtype for output (e.g., uint8, uint16). Default: uint16', default="uint16", action=SM)
 
     general = parser.add_argument_group('General arguments')
     general.add_argument('-v', '--verbose', help='Increase verbosity. Default: False', action='store_true', default=False)
@@ -48,13 +47,18 @@ def main():
     Configuration.verbose = args.verbose
     verbose_start_msg()
 
-    forward_warp(args.fixed_img, args.reg_outputs, args.fixed_reg_in, args.moving_img, args.interpol, output=args.output, pad_percent=args.pad_percent)
+    reg_path = Path(args.fixed_reg_in).parent
+    img = load_nii(args.moving_img)
 
-    # Delete the intermediate warped image
-    warp_outputs_dir = Path(args.reg_outputs) / "warp_outputs" 
-    warped_nii_path = Path(str(warp_outputs_dir / str(Path(args.moving_img).name).replace(".nii.gz", "_in_fixed_img_space.nii.gz")))
-    if warped_nii_path.exists():
-        warped_nii_path.unlink()
+    # Warp an image from Allen CCFv3 30 um space to gubra 25 um space
+    to_atlas(reg_path, 
+             img, 
+             args.fixed_reg_in, 
+             args.atlas, 
+             args.output, 
+             args.interpol, 
+             dtype=args.dtype, 
+             pad_percent=args.pad_percent)
 
     verbose_end_msg()
 
