7597c866482608972e1f3a6f2290386bb95b3c14
daniel-rijsketic <daniel.rijsketic@gmail.com>
2025-06-09 16:57:49 -0600

    Working draft of RNAseq_join_expression_data.py complete, but could move some processing to RNAseq_expression.py

--- _other/drafts/_ABCA/RNAseq_join_expression_data.py
+++ _other/drafts/_ABCA/RNAseq_join_expression_data.py
@@ -5,7 +5,10 @@ Use ``./RNAseq_join_expression_data.py`` from UNRAVEL to join cell metadata with
 
 Prereqs:
     - RNAseq_expression.py script to generate the input expression data.
-    - Cell metadata from the Allen Brain Cell Atlas.
+    - Cell metadata from the Allen Brain Cell Atlas (use ``abca_cache`` to download).
+
+Output:
+    - A CSV file with the joined data (<input>_joined_expression.csv)
 
 Usage:
 ------
@@ -13,11 +16,7 @@ Usage:
 
 """
 
-import anndata
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import SimpleITK as sitk
 from pathlib import Path
 from rich import print
 from rich.traceback import install
@@ -37,11 +36,15 @@ def parse_args():
     reqs.add_argument('-i', '--input', help='path/RNAseq_expression.csv', required=True, action=SM)
     reqs.add_argument('-s', '--species', help='Species to analyze (e.g., "mouse" or "human")', required=True, action=SM)
 
+    opts = parser.add_argument_group('Optional arguments')
+    opts.add_argument('-o', '--output', help='Output path for the joined cell metadata and expression data.', default=None, action=SM)
+
     general = parser.add_argument_group('General arguments')
     general.add_argument('-v', '--verbose', help='Increase verbosity. Default: False', action='store_true', default=False)
 
     return parser.parse_args()
 
+# TODO: Move cell metadata loading, classification, and color joining to RNAseq_expression.py
 
 @log_command
 def main():
@@ -54,30 +57,30 @@ def main():
 
     cell_df = load_RNAseq_cell_metadata(download_base, species=args.species)
 
-    print(cell_df)
-
     # Classify cells based on their metadata
     cell_df = mf.join_cluster_details(cell_df, download_base, args.species)  # This could be moved to the RNAseq_expression.py script
-
-    print(cell_df)
 
     # Add color info
     cell_df = mf.join_cluster_colors(cell_df, download_base, args.species)
 
-    print(cell_df)
-
     # Load the input expression data (from RNAseq_expression.py)
     exp_df = pd.read_csv(args.input, dtype={'cell_label': str})
+    exp_df = exp_df.set_index('cell_label')
+    exp_cols = exp_df.columns.tolist()
 
-    print(exp_df)
+    # Join the expression data with the cell metadata
+    exp_df = exp_df.join(cell_df, on='cell_label', how='left')
 
-    # Join the cell metadata with the expression data using the 'cell_label' column
-    exp_df = cell_df.join(exp_df.set_index('cell_label'), how='left').reset_index()
-
-    print(exp_df)
+    # Move the exp_cols to the end of the DataFrame
+    exp_df = exp_df[[col for col in exp_df.columns if col not in exp_cols] + exp_cols]
 
     # Save the joined data
-    exp_df.to_csv(f"{Path(args.input).stem}_w_cells.csv", index=False)
+    if args.output is not None:
+        output_path = Path(args.output)
+    else:
+        output_path = Path(args.input).with_suffix('_joined_expression.csv')
+    exp_df.to_csv(output_path, index=False)
+    print(f"\n    Saved the joined data to {output_path}\n")
 
     verbose_end_msg()
 
