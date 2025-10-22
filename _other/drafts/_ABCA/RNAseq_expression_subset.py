2ecd415e8ef99866becb252af0e72433a9e904c4
daniel-rijsketic <daniel.rijsketic@gmail.com>
2025-10-21 22:51:29 -0600

    Refactor RNAseq expression subset script: update usage instructions, normalize gene names, and improve error handling for cell types and gene metadata.

--- _other/drafts/_ABCA/RNAseq_expression_subset.py
+++ _other/drafts/_ABCA/RNAseq_expression_subset.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python3
 
 """
-Use ``abca_scRNAseq_expression_subset`` or ``rna_exp`` from UNRAVEL to quickly load a subset of genes from scRNA-seq expression data from the Allen Brain Cell Atlas.
+Use ``abca_scRNAseq_expression_subset`` or ``rna_subset`` from UNRAVEL to quickly load a subset of genes from scRNA-seq expression data from the Allen Brain Cell Atlas.
+
+Prereqs:
+    - Use ``abca_cache`` to download the expression matrices and metadata.
+    - Use h5ad_to_dense_zarr.py to convert the h5ad files to dense Zarr format for faster loading of genes (chunks optimized for gene-wise access).
+
+Output:
+    - A CSV file with the expression data for the selected genes, indexed by cell_label.
+    - Cell metadata columns: feature_matrix_label, region_of_interest_acronym, x, y, cluster_alias.    
 
 Note:
     - https://alleninstitute.github.io/abc_atlas_access/notebooks/general_accessing_10x_snRNASeq_tutorial.html
-    - Only the first gene in the list will be used to name the output file.
-    - For humans, the cell type must be specified (Neurons or Nonneurons).
-    - For mice, the region must be specified (OLF, CTXsp, Isocortex-1, Isocortex-2, HPF, STR, PAL, TH, HY, MB, MY, P, CB).
-    - The output will be a CSV file with the expression data for the selected genes, indexed by cell_label.
+
+Next steps:
+    - ``abca_scRNAseq_filter`` to filter cells based on metadata.
+    - ``abca_sunburst`` to generate sunburst plots of cell type proportions.
+    - ``abca_sunburst_expression`` to generate sunburst plots of gene expression.
 
 Usage:
 ------
-    abca_scRNAseq_expression -b path/base_dir -g genes [-s mouse | human] [-c Neurons | Nonneurons] [-r region] [-d log2 | raw ] [-o output] [-v]
+    abca_scRNAseq_expression -b path/base_dir -g genes [-s mouse | human] [-c neurons | nonneurons] [-d log2 | raw ] [-o output] [-v]
 
 Usage for humans:
 -----------------
-    abca_scRNAseq_expression -b path/base_dir -g genes -c Neurons [-o output_dir] [-v]
+    abca_scRNAseq_expression -b path/base_dir -g genes -c neurons [-o output_dir] [-v]
 
 Usage for mice:
 ---------------
-    abca_scRNAseq_expression -b path/base_dir -g genes -r region [-o output_dir] [-v]
+    abca_scRNAseq_expression -b path/base_dir -g genes [-o output_dir] [-v]
 """
 
 from typing import List
@@ -42,13 +51,11 @@ def parse_args():
 
     reqs = parser.add_argument_group('Required arguments')
     reqs.add_argument('-b', '--base', help='Path to the root directory of the Allen Brain Cell Atlas data', required=True, action=SM)
-    reqs.add_argument('-g', '--genes', help='Genes to extract expression data for.', nargs='*', required=True, action=SM)
+    reqs.add_argument('-g', '--genes', help='Space-separated list of genes to extract expression data for.', nargs='*', required=True, action=SM)
     
     opts = parser.add_argument_group('Optional arguments')
-    opts.add_argument('-s', '--species', help='Species to use (human or mouse). Default: human', default='human', action=SM)
-    opts.add_argument('-c', '--cell_type', help='Cell type to extract data from for humans (Neurons \[default] or Nonneurons)', default='Neurons', action=SM)
-    opts.add_argument('-r', '--region', help='Region to use for mice (OLF, CTXsp, Isocortex-1, Isocortex-2, HPF, STR, PAL, TH, HY, MB, MY, P, CB). Default: None', default=None, action=SM)
-    opts.add_argument('-d', '--data_type', help='Type of expression data (log2 or raw). Default: log2', default='log2', action=SM)
+    opts.add_argument('-s', '--species', help='Species to use (human or mouse). Default: mouse', default='mouse', action=SM)
+    opts.add_argument('-c', '--cell_type', help='Cell type to use (neurons or nonneurons). Default: None', default=None, action=SM)
     opts.add_argument('-o', '--output', help='Path to output CSV file.', default=None, action=SM)
 
     general = parser.add_argument_group('General arguments')
@@ -57,7 +64,6 @@ def parse_args():
     return parser.parse_args()
 
 # TODO: Should load_RNAseq_cell_metadata and load_RNAseq_gene_metadata be moved to a separate module?
-# TODO: loading expression data is slow (loads whoe dataset). It might be optimized by changing the orientation of the data (CSR to CSC) once, and then perhaps slices of data can be loaded instead of the whole dataset.
 
 def load_RNAseq_cell_metadata(download_base, species='mouse'):
     """
@@ -68,7 +74,7 @@ def load_RNAseq_cell_metadata(download_base, species='mouse'):
     download_base : Path
         The base directory where the data is downloaded.
     species : str
-        The species to use (human or mouse). Default: 'human'.
+        The species to use (human or mouse). Default: 'mouse'.
 
     Returns
     -------
@@ -101,7 +107,7 @@ def load_RNAseq_gene_metadata(download_base, species='mouse'):
     download_base : Path
         The base directory where the data is downloaded.
     species : str
-        The species to use (human or mouse). Default: 'human'.
+        The species to use (human or mouse). Default: 'mouse'.
 
     Returns
     -------
@@ -119,91 +125,6 @@ def load_RNAseq_gene_metadata(download_base, species='mouse'):
     else:
         raise FileNotFoundError(f"\nGene metadata not found at {gene_metadata_path}\n")
     return gene_df
-
-
-def get_gene_data_wo_cache_and_chunking(
-    download_base: Path,
-    all_cells: pd.DataFrame,
-    all_genes: pd.DataFrame,
-    selected_genes: List[str],
-    data_type: str = "log2",
-    species: str = "human",
-    region: str = None,
-    cell_type: str = None
-) -> pd.DataFrame:
-    """Load and structure gene expression data directly from RNA-seq data for specific genes.
-    
-    Parameters
-    ----------
-    download_base : Path
-        The base directory where the data is located.
-    all_cells : pandas.DataFrame
-        Cell metadata indexed on cell_label.
-    all_genes : pandas.DataFrame
-        Gene metadata indexed on gene_identifier.
-    selected_genes : list of strings
-        List of gene_symbols that are a subset of those in the full genes DataFrame.
-    data_type : str
-        Type of expression data, "log2" or "raw". Defaults to "log2".
-    species : str
-        The species to use (human or mouse). Default: 'human'.
-    region : str
-        The region to use for mice (OLF, CTXsp, Isocortex-1, Isocortex-2, HPF, STR, PAL, TH, HY, MB, MY, P, CB). Default: None.
-    cell_type : str
-        The cell type to use for humans (Neurons or Nonneurons). Default: None.
-
-    Returns
-    -------
-    output_gene_data : pandas.DataFrame
-        Subset of gene data indexed by cell.
-    """
-    # Filter genes
-    gene_mask = all_genes.gene_symbol.isin(selected_genes)
-    gene_filtered = all_genes[gene_mask]
-    if gene_filtered.empty:
-        print(f"    [red1]Error: None of the selected genes ({selected_genes}) found in gene metadata.\n")
-        import sys; sys.exit()
-    
-    print(f"\n    Selected genes in gene metadata: {gene_filtered['gene_symbol'].tolist()}\n")
-    
-    # Path to expression data
-    if species == 'mouse':
-        expression_path = download_base / f"expression_matrices/WMB-10Xv3/20230630/WMB-10Xv3-{region}-{data_type}.h5ad"
-    else:
-        expression_path = download_base / f"expression_matrices/WHB-10Xv3/20240330/WHB-10Xv3-{cell_type}-{data_type}.h5ad"
-    
-    if not expression_path.exists():
-        print(f"[red1]Error: Expression data not found at {expression_path}\n")
-        import sys; sys.exit()
-    
-    print(f"    Loading expression data from {expression_path}")
-    expression_data = anndata.read_h5ad(expression_path)
-    print("    Data loaded successfully.\n")
-    
-    # Match cells between metadata and expression data
-    print("    Matching cell labels between metadata and expression data...")
-    cell_indexes = all_cells.index.intersection(expression_data.obs_names)  # Check for matching cell labels
-    if len(cell_indexes) == 0:
-        print("\n    [red1]No matching cell labels found. Please check label formats.\n")
-        import sys; sys.exit()
-    
-    print(f"    Number of matching cells: {len(cell_indexes)}")
-    
-    # Extract expression data for the selected cells and genes
-    try:
-        print(f"    Extracting expression data for the selected cells and genes...")
-        expression_subset = expression_data[cell_indexes, gene_filtered.index].to_df()
-        expression_subset.columns = gene_filtered.gene_symbol
-        expression_subset.index.name = 'cell_label'
-        print(f"    Extracted expression data:\n\n{expression_subset.head()}\n")
-    except KeyError as e:
-        print(f"\n    [red1]Error extracting data: {e}\n")
-        import sys; sys.exit()
-
-    if hasattr(expression_data, 'file'):
-        expression_data.file.close()  # Close file only if backed mode is used
-
-    return expression_subset.reset_index()
 
 def load_expression_from_zarr(file, gene_list):
     """Load and return a DataFrame of selected genes from a single Zarr file."""
@@ -223,24 +144,53 @@ def main():
     download_base = Path(args.base)
 
     if args.species == 'human':
-        valid_cell_types = {"Neurons", "Nonneurons"}
+        valid_cell_types = {"neurons", "nonneurons"}
         if args.cell_type is None or args.cell_type not in valid_cell_types:
             print(f"\n    [red1]Error: Please provide a valid cell type: {valid_cell_types}\n")
             return
-        
+
     # Load gene metadata. Index: gene_identifier (e.g., ENSMUSG00000034997). Columns: gene_symbol (e.g., Htr2a)
     gene_df = load_RNAseq_gene_metadata(download_base, species=args.species)
     gene_list = [g for g in args.genes if g in gene_df["gene_symbol"].values]
+
+    # Normalize gene names based on species conventions
+    if args.species == 'mouse':
+        # Mouse gene symbols: capitalize only the first letter (Htr2a)
+        gene_list = [g.lower().capitalize() for g in gene_list]
+    elif args.species == 'human':
+        # Human gene symbols: all uppercase (HTR2A)
+        gene_list = [g.upper() for g in gene_list]
+
     if not gene_list:
         raise ValueError("None of the requested genes found in gene metadata.")
-
 
     # Load cell metadata. Index: cell_label. Columns: feature_matrix_label, region_of_interest_acronym, x, y, cluster_alias
     cell_df = load_RNAseq_cell_metadata(download_base, species=args.species)
 
-    # Load expression data from each file and concatenate
-    expression_matrices_dir = download_base / 'expression_matrices'
-    pattern = "**/WMB-10Xv3-*-log2.zarr" if args.species == "mouse" else "**/WHB-10Xv3-*-log2.zarr"
+    # Define the directory containing expression matrices
+    expression_matrices_dir = download_base / "expression_matrices"
+    ct = args.cell_type.lower() if args.cell_type else None
+
+    # Pattern to match files based on species and cell type
+    if args.species == "mouse":
+        pattern = "**/WMB-10Xv3-*-log2.zarr"
+
+    # Load human data for neurons, nonneurons, or all cells
+    elif args.species == "human":
+        if args.cell_type is None:
+            # load all if unspecified
+            pattern = "**/WHB-10Xv3-*-log2.zarr"
+        else:
+            
+            if ct == "neurons":
+                pattern = "**/WHB-10Xv3-Neurons-log2.zarr"
+            elif ct == "nonneurons":
+                pattern = "**/WHB-10Xv3-Nonneurons-log2.zarr"
+            else:
+                raise ValueError("For human data, --cell_type must be 'neurons', 'nonneurons', or None.")
+    else:
+        raise ValueError("Species must be 'mouse' or 'human'.")
+
     zarr_files = list(expression_matrices_dir.rglob(pattern))  # Converted with h5ad_to_dense_zarr.py
     exp_dfs = []
     for file in zarr_files:
@@ -259,11 +209,18 @@ def main():
         raise ValueError("No expression data loaded from Zarr files. Check file paths.")
     
     full_exp_df = pd.concat(exp_dfs, axis=0)
+    print(f"\n    Full expression data:\n{full_exp_df}\n")
 
     # Join with cell metadata
-    print(f"\n    Full expression data:\n{full_exp_df.head()}\n")
     cell_df_joined = cell_df.join(full_exp_df, how='inner')
 
+    # Filter by cell type for mice if specified
+    if args.species == 'mouse' and ct == 'neurons':
+        cell_df_joined = cell_df_joined[cell_df_joined['class'].str.split().str[0].astype(int) <= 29]
+    elif args.species == 'mouse' and ct == 'nonneurons':
+        cell_df_joined = cell_df_joined[cell_df_joined['class'].str.split().str[0].astype(int) > 29]
+
+    print(f"\n    Joined cell metadata:\n{cell_df_joined}\n")
 
     # Define output file path and save the DataFrame
     if args.output: 
