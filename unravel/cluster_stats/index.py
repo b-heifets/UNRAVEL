c7082c7d17d554fa269998a0608e54cf46c29208
daniel-rijsketic <daniel.rijsketic@gmail.com>
2026-03-05 17:25:34 -0700

    fix: update TODO comments to clarify future enhancements for cluster ID handling

--- unravel/cluster_stats/index.py
+++ unravel/cluster_stats/index.py
@@ -49,6 +49,7 @@ def parse_args():
     return parser.parse_args()
 
 # TODO: Look into consolidating csvs 
+# TODO: make default for -ids all clusters. Perhaps make this less specific to valid cluster and more general.
 
 
 def generate_sunburst(cluster, img, atlas, xyz_res_in_um, data_type, output_dir, sunburst_csv_path, info_csv_path, output_rgb_lut):
