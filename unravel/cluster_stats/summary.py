88fad4b382697674aff8090c4ae227513e974f1b
daniel-rijsketic <daniel.rijsketic@gmail.com>
2026-03-13 11:10:19 -0600

    refactor(summary): add TODO for logging segmentation details in output directory

--- unravel/cluster_stats/summary.py
+++ unravel/cluster_stats/summary.py
@@ -113,6 +113,7 @@ def parse_args():
 # TODO: Could make it possible to generate data for all clusters, not just valid clusters
 # TODO: Given that the cluster_index_dir and cluster_info.txt names should be related, could add a check for this (perhaps also simplify logic for finding the cluster_info.txt file)
 # TODO: cluster_mean_IF_summary_ttest.csv has the p-value column named as p-adj even for t-tests. Could change this to p_val
+# TODO: Include some form of logging regarding the segmentation that was used in sample??/clusters/output_dir (in the dir name, output file names, or in a .txt file). Currently if a new segmentation is used, old outputs must be moved to a new directory to avoid confusion.
 
 def run_script(script_name, script_args):
     """Run a command/script using subprocess that respects the system's PATH and captures output."""
