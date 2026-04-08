617b6490cf2a284b4f61a424128f7e451209a4ed
daniel-rijsketic <daniel.rijsketic@gmail.com>
2026-04-08 10:48:53 -0600

    fix(auto_crop): correct unpacking of largest_component_bbox_coronal to improve clarity

--- unravel/allen_institute/genetic_tools_atlas/auto_crop.py
+++ unravel/allen_institute/genetic_tools_atlas/auto_crop.py
@@ -161,7 +161,8 @@ def main():
             largest_component_bbox_sagittal = stats_sagittal['bounding_boxes'][largest_label_sagittal]
 
             # bbox is (x_slice, y_slice, z_slice)
-            y_slice, x_slice, _ = largest_component_bbox_coronal
+            # y_slice, x_slice, _ = largest_component_bbox_coronal
+            y_slice, x_slice = largest_component_bbox_coronal
             xmin, xmax = x_slice.start, x_slice.stop
             ymin, ymax = y_slice.start, y_slice.stop
 
