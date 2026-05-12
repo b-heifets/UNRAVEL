e9be180d8bc4e52ad208d79b201cbcc883b98cdb
daniel-rijsketic <daniel.rijsketic@gmail.com>
2026-05-12 15:17:25 -0600

    fix(summary): update import for get_matching_input_csvs from cstats module

--- unravel/cluster_stats/summary.py
+++ unravel/cluster_stats/summary.py
@@ -66,9 +66,9 @@ from pathlib import Path
 from rich import print
 from rich.traceback import install
 
-from unravel.cluster_stats.org_data import cp, get_matching_input_csvs
+from unravel.cluster_stats.org_data import cp
+from unravel.cluster_stats.cstats import get_matching_input_csvs
 from unravel.core.help_formatter import RichArgumentParser, SuppressMetavar, SM
-
 from unravel.core.config import Configuration 
 from unravel.core.utils import log_command, verbose_start_msg, verbose_end_msg, load_config
 from unravel.utilities.aggregate_files_recursively import find_and_copy_files
